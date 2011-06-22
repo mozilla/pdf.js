4281f799e5926d7087da69809d48ebe2ecfc7425
Julian Viereck <julian.viereck@gmail.com>
2011-06-22 22:54:16 +0200

    Move client code into worker_client.js. Cleanup + comments + 2-space-indention

--- worker.js
+++ worker.js
@@ -1,15 +1,26 @@
 "use strict";
 
+var timer = null;
+function tic() {
+  timer = Date.now();
+}
+
+function toc(msg) {
+  log(msg + ": " + (Date.now() - timer) + "ms");
+  timer = null;
+}
+
 function log() {
-    var args = Array.prototype.slice.call(arguments);
-    postMessage("log");
-    postMessage(JSON.stringify(args))
+  var args = Array.prototype.slice.call(arguments);
+  postMessage("log");
+  postMessage(JSON.stringify(args))
 }
 
 var console = {
-    log: log
+  log: log
 }
 
+//
 importScripts("canvas_proxy.js");
 importScripts("pdf.js");
 importScripts("fonts.js");
@@ -18,55 +29,50 @@ importScripts("glyphlist.js")
 // Use the JpegStreamProxy proxy.
 JpegStream = JpegStreamProxy;
 
-var timer = null;
-function tic() {
-    timer = Date.now();
-}
-
-function toc(msg) {
-    log(msg + ": " + (Date.now() - timer) + "ms");
-    timer = null;
-}
-
 // Create the WebWorkerProxyCanvas.
 var canvas = new CanvasProxy(1224, 1584);
 
-var pageInterval;
+// Listen for messages from the main thread.
 var pdfDocument = null;
 onmessage = function(event) {
-    var data = event.data;
-    if (!pdfDocument) {
-        pdfDocument = new PDFDoc(new Stream(data));
-        postMessage("pdf_num_page");
-        postMessage(pdfDocument.numPages)
-        return;
-    } else {
-        tic();
+  var data = event.data;
+  // If there is no pdfDocument yet, then the sent data is the PDFDocument.
+  if (!pdfDocument) {
+    pdfDocument = new PDFDoc(new Stream(data));
+    postMessage("pdf_num_page");
+    postMessage(pdfDocument.numPages)
+    return;
+  }
+  // User requested to render a certain page.
+  else {
+    tic();
 
-        // Let's try to render the first page...
-        var page = pdfDocument.getPage(parseInt(data));
+    // Let's try to render the first page...
+    var page = pdfDocument.getPage(parseInt(data));
 
-        // page.compile will collect all fonts for us, once we have loaded them
-        // we can trigger the actual page rendering with page.display
-        var fonts = [];
-        var gfx = new CanvasGraphics(canvas.getContext("2d"), CanvasProxy);
-        page.compile(gfx, fonts);
+    // page.compile will collect all fonts for us, once we have loaded them
+    // we can trigger the actual page rendering with page.display
+    var fonts = [];
+    var gfx = new CanvasGraphics(canvas.getContext("2d"), CanvasProxy);
+    page.compile(gfx, fonts);
 
-        // Inspect fonts and translate the missing one.
-        var count = fonts.length;
-        for (var i = 0; i < count; i++) {
-          var font = fonts[i];
-          if (Fonts[font.name]) {
-            fontsReady = fontsReady && !Fonts[font.name].loading;
-            continue;
-          }
+    // Inspect fonts and translate the missing one.
+    var count = fonts.length;
+    for (var i = 0; i < count; i++) {
+      var font = fonts[i];
+      if (Fonts[font.name]) {
+        fontsReady = fontsReady && !Fonts[font.name].loading;
+        continue;
+      }
 
-          // This "builds" the font and sents it over to the main thread.
-          new Font(font.name, font.file, font.properties);
-        }
-        toc("compiled page");
-
-        page.display(gfx);
-        canvas.flush();
+      // This "builds" the font and sents it over to the main thread.
+      new Font(font.name, font.file, font.properties);
     }
+    toc("compiled page");
+
+    tic()
+    page.display(gfx);
+    canvas.flush();
+    toc("displayed page");
+  }
 }
