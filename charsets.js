5a3d85bf53341078186d7fda8688efab921e610f
Kalervo Kujala <kkujala@com>
2011-10-15 14:20:15 +0300

    Fix small lint issues.

--- charsets.js
+++ charsets.js
@@ -1,3 +1,7 @@
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+
+'use strict';
 
 var ISOAdobeCharset = [
   '.notdef', 'space', 'exclam', 'quotedbl', 'numbersign', 'dollar',
