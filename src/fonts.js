315b089b28fcf8e0f7ff04b6c2596c0553b98405
notmasteryet <async.processingjs@yahoo.com>
2011-12-09 21:21:58 -0600

    (#919) Fixing toUnicode mapping for TrueType fonts with identity mapping

--- src/fonts.js
+++ src/fonts.js
@@ -1791,6 +1791,10 @@ var Font = (function FontClosure() {
               var i = unassignedUnicodeItems[j];
               while (unusedUnicode in usedUnicodes)
                 unusedUnicode++;
+              var cid = i + 1;
+              // override only if unicode mapping is not specified
+              if (!(cid in toUnicode))
+                toUnicode[cid] = unusedUnicode;
               glyphs[i].unicode = unusedUnicode++;
             }
             this.useToUnicode = true;
