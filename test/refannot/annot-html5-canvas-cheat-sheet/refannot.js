be1ec63a48cb29b0053cad6c5d3b5460e272cd27
Joe Devietti <devietti@cs.washington.edu>
2012-01-13 11:53:56 -0800

    allow testing of text/highlight/underline annotation extraction

---
+++ test/refannot/annot-html5-canvas-cheat-sheet/refannot.js
@@ -0,0 +1,8 @@
+{ "1":[{"markup": [" Canvas "], "type": "Highlight"}, 
+       {"content": "Here's a note.", "type": "Text"}, 
+       {"markup": ["Supports any of the fol"], "type": "Highlight"}, 
+       {"markup": ["e HTMLImageElement"], "type": "Underline"}
+      ],
+  "2":[{"markup": ["quadraticCurveTo(","float cpx, float cpy,","float x, float y )"], "type":"Highlight"}
+      ]
+}
