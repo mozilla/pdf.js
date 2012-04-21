f6d77e53bdeb85b5bd9b5d1b701da7abcaa11ce4
gigaherz <gigaherz@gmail.com>
2012-04-21 14:14:56 +0200

    Forgot to add the license files and license header for jbig2dec.

--- external/jbig2dec/jbig2dec.js
+++ external/jbig2dec/jbig2dec.js
@@ -1,3 +1,18 @@
+/*
+    jbig2dec
+
+    Copyright (C) 2002-2008 Artifex Software, Inc.
+
+    This software is distributed under license and may not
+    be copied, modified or distributed except as expressly
+    authorized under the terms of the license contained in
+    the file LICENSE in this distribution.
+
+    For further licensing information refer to http://artifex.com/ or
+    contact Artifex Software, Inc., 7 Mt. Lassen Drive - Suite A-134,
+    San Rafael, CA  94903, U.S.A., +1(415)492-9861.
+*/
+
 function N(oa) {
   throw oa
 }
