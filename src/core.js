278dc81a5404377889af7f6ecaf4bebb7e4b7304
benbro <ben.browitt@gmail.com>
2012-07-13 18:00:55 +0300

    Move the try/catch block deeper

--- src/core.js
+++ src/core.js
@@ -398,16 +398,16 @@ var PDFDocument = (function PDFDocumentClosure() {
     get linearization() {
       var length = this.stream.length;
       var linearization = false;
-      try {
-        if (length) {
+      if (length) {
+        try {
           linearization = new Linearization(this.stream);
           if (linearization.length != length)
             linearization = false;
+        } catch (err) {
+          warn('since pdf is broken pdf.js is trying to recover it ' +
+               'by indexing the object; ' +
+               'the error in firebug shall have a different origin');
         }
-      } catch (err) {
-        warn('since pdf is broken pdf.js is trying to recover it ' +
-             'by indexing the object; ' +
-             'the error in firebug shall have a different origin');
       }
       // shadow the prototype getter with a data property
       return shadow(this, 'linearization', linearization);
