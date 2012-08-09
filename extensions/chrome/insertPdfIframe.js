abcf6abc71a4a5ebc45564b2f53f33a18cf46693
Andres Riofrio <riofrios@gmail.com>
2012-08-09 00:42:29 -0700

    Use requestAnimationFrame to remove replacement flicker

--- extensions/chrome/insertPdfIframe.js
+++ extensions/chrome/insertPdfIframe.js
@@ -10,12 +10,19 @@ var viewerPage = 'content/web/viewer.html';
 var url = chrome.extension.getURL(viewerPage) +
   '?file=' + encodeURIComponent(document.location.href);
 
-var embed = document.querySelector('embed');
-var iframe = document.createElement('iframe');
-iframe.src = url;
-iframe.style.width = '100%';
-iframe.style.height = '100%';
-iframe.style.border = 0;
-embed.parentNode.replaceChild(iframe, embed);
+function tryInsertIframe() {
+  var embed = document.querySelector('embed');
+  if(embed) {
+    var iframe = document.createElement('iframe');
+    iframe.src = url;
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.border = 0;
+    embed.parentNode.replaceChild(iframe, embed);
+  } else {
+    window.requestAnimationFrame(tryInsertIframe);
+  }
+}
+window.webkitRequestAnimationFrame(tryInsertIframe);
 
 // TODO: Synchronize title, focus iframe.
