cc2e193493f7c111456d6683bbf2b071cdd53f0e
Pimm Hogeling <pimmhogeling@gmail.com>
2012-08-29 23:20:19 +0200

    Changed syntax of the log function, to prevent minifiers/compilers from doing illegal renames.

--- src/util.js
+++ src/util.js
@@ -6,10 +6,10 @@
 // Use only for debugging purposes. This should not be used in any code that is
 // in mozilla master.
 function log(msg) {
-  if (console && console.log)
-    console.log(msg);
-  else if (print)
-    print(msg);
+  if ("console" in globalScope && "log" in globalScope["console"])
+    globalScope["console"]["log"](msg);
+  else if ("print" in globalScope)
+    globalScope["print"](msg);
 }
 
 // A notice for devs that will not trigger the fallback UI.  These are good
