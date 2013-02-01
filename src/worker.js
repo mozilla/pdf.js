724add2ffde5ce10e98764448c0c2b986e4d4417
vyv03354 <VYV03354@nifty.ne.jp>
2013-02-02 02:09:12 +0900

    Make an effort to adhere the spec 7.6.3.2

--- src/worker.js
+++ src/worker.js
@@ -212,11 +212,13 @@ var WorkerMessageHandler = {
     handler.on('GetPageRequest', function wphSetupGetPage(data) {
       var pageNumber = data.pageIndex + 1;
       var pdfPage = pdfModel.getPage(pageNumber);
+      var encrypt = pdfModel.xref.encrypt;
       var page = {
         pageIndex: data.pageIndex,
         rotate: pdfPage.rotate,
         ref: pdfPage.ref,
-        view: pdfPage.view
+        view: pdfPage.view,
+        disableTextLayer: encrypt ? encrypt.disableTextLayer : false
       };
       handler.send('GetPage', {pageInfo: page});
     });
