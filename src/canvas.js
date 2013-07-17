a74cd153d1f45b9da99a28752463625ab393248f
Srishti <srishsensation@gmail.com>
2013-07-17 13:02:26 +0530

    test

--- src/canvas.js
+++ src/canvas.js
@@ -1198,13 +1198,13 @@ var CanvasGraphics = (function CanvasGraphicsClosure() {
 
       if (textSelection) {
         geom.canvasWidth = canvasWidth;
-        if (vertical) {
+     /*   if (vertical) {
           var vmetric = font.defaultVMetrics;
           geom.x += vmetric[1] * fontSize * current.fontMatrix[0] /
                     fontSizeScale * geom.hScale;
           geom.y += vmetric[2] * fontSize * current.fontMatrix[0] /
                     fontSizeScale * geom.vScale;
-        }
+        }*/
         this.textLayer.appendText(geom);
       }
 
