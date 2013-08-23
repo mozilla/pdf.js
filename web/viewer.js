8892beeb085def95d33b2e06eb52571c781772c7
Jonas <Snuffleupagus@server.fake>
2013-08-23 13:08:51 +0200

    Remove old dead find related code from viewer.js

--- web/viewer.js
+++ web/viewer.js
@@ -174,8 +174,6 @@ var PDFView = {
   currentScale: UNKNOWN_SCALE,
   currentScaleValue: null,
   initialBookmark: document.location.hash.substring(1),
-  startedTextExtraction: false,
-  pageText: [],
   container: null,
   thumbnailContainer: null,
   initialized: false,
@@ -864,8 +862,6 @@ var PDFView = {
     this.pageRotation = 0;
 
     var pages = this.pages = [];
-    this.pageText = [];
-    this.startedTextExtraction = false;
     var pagesRefMap = this.pagesRefMap = {};
     var thumbnails = this.thumbnails = [];
 
