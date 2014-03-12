0fa44d7233bb4190099ac96e24efe21155014f93
Thorben Bochenek <thorbenb@opera.com>
2014-03-12 09:51:40 +0100

    refactor src/core/pdf_manager.js: rename pdfModel to pdfDocument
    Rename pdfModel to pdfDocument to let the name closer resemble what the
    variable actually holds

--- src/core/worker.js
+++ src/core/worker.js
@@ -294,7 +294,8 @@ var WorkerMessageHandler = PDFJS.WorkerMessageHandler = {
 
     handler.on('GetPageIndex', function wphSetupGetPageIndex(data, deferred) {
       var ref = new Ref(data.ref.num, data.ref.gen);
-      pdfManager.pdfModel.catalog.getPageIndex(ref).then(function (pageIndex) {
+      var catalog = pdfManager.pdfDocument.catalog;
+      catalog.getPageIndex(ref).then(function (pageIndex) {
         deferred.resolve(pageIndex);
       }, deferred.reject);
     });
