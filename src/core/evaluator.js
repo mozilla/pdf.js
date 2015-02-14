mozilla/pdf.js

commit a468f7215dbc6428d01589f2ecf5595308898a7d

Fixes spacing in the text layer builder

src/core/evaluator.js
@@ -765,6 +765,7 @@ var PartialEvaluator = (function PartialEvaluatorClosure() {
         for (var i = 0; i < glyphs.length; i++) {
           var glyph = glyphs[i];
           if (!glyph) { // Previous glyph was a space.
+            width += textState.wordSpacing * textState.textHScale;
             continue;
           }
           var vMetricX = null;
@@ -870,10 +871,10 @@ var PartialEvaluator = (function PartialEvaluatorClosure() {
                                         args[4], args[5]);
             break;
           case OPS.setCharSpacing:
-            textState.charSpace = args[0];
+            textState.charSpacing = args[0];
             break;
           case OPS.setWordSpacing:
-            textState.wordSpace = args[0];
+            textState.wordSpacing = args[0];
             break;
           case OPS.beginText:
             textState.textMatrix = IDENTITY_MATRIX.slice();
@@ -889,11 +890,12 @@ var PartialEvaluator = (function PartialEvaluatorClosure() {
               } else {
                 var val = items[j] / 1000;
                 if (!textState.font.vertical) {
-                  offset = -val * textState.fontSize * textState.textHScale;
+                  offset = -val * textState.fontSize * textState.textHScale *
+                           textState.textMatrix[0];
                   textState.translateTextMatrix(offset, 0);
                   textChunk.width += offset;
                 } else {
-                  offset = -val * textState.fontSize;
+                  offset = -val * textState.fontSize * textState.textMatrix[3];
                   textState.translateTextMatrix(0, offset);
                   textChunk.height += offset;
                 }

commit 8010dc815459c53278c5d6d75e8abbc9d49779a4

Fix regression introduced by a468f7215dbc6428d01589f2ecf5595308898a7d

src/core/evaluator.js
@@ -1111,15 +1111,13 @@ var PartialEvaluator = (function PartialEvaluatorClosure() {
                 } else {
                   var val = items[j] / 1000;
                   if (!textState.font.vertical) {
-                    offset = -val * textState.fontSize * textState.textHScale *
-                      textState.textMatrix[0];
+                    offset = -val * textState.fontSize * textState.textHScale;
                     textState.translateTextMatrix(offset, 0);
-                    textChunk.width += offset;
+                    textChunk.width += offset * textState.textMatrix[0];
                   } else {
-                    offset = -val * textState.fontSize *
-                      textState.textMatrix[3];
+                    offset = -val * textState.fontSize;
                     textState.translateTextMatrix(0, offset);
-                    textChunk.height += offset;
+                    textChunk.height += offset * textState.textMatrix[3];
                   }
                   if (items[j] < 0 && textState.font.spaceWidth > 0) {
                     var fakeSpaces = -items[j] / textState.font.spaceWidth;