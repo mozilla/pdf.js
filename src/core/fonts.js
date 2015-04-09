fda858ae336417fdb4936ce5ff28059df79623de
Jonas Jenwald <jonas.jenwald@gmail.com>
2015-04-09 16:09:24 +0200

    Don't map glyphs to certain problematic General Punctuation Unicode locations (bug 911034)
    Fixes the remaining missing characters in https://bugzilla.mozilla.org/show_bug.cgi?id=911034.
    
    For reference, see http://www.unicode.org/charts/PDF/U2000.pdf (and also http://en.wikipedia.org/wiki/General_Punctuation_%28Unicode_block%29).

--- src/core/fonts.js
+++ src/core/fonts.js
@@ -2666,6 +2666,11 @@ var Font = (function FontClosure() {
     if (code >= 0x80 && code <= 0x9F) { // Control chars
       return true;
     }
+    if ((code >= 0x2000 && code <= 0x200F) || // General punctuation chars
+        (code >= 0x2028 && code <= 0x202F) ||
+        (code >= 0x2060 && code <= 0x206F)) {
+      return true;
+    }
     if (code >= 0xFFF0 && code <= 0xFFFF) { // Specials Unicode block
       return true;
     }
@@ -2674,6 +2679,8 @@ var Font = (function FontClosure() {
       case 0xA0: // Non breaking space
       case 0xAD: // Soft hyphen
       case 0x0E33: // Thai character SARA AM
+      case 0x2011: // Non breaking hyphen
+      case 0x205F: // Medium mathematical space
       case 0x25CC: // Dotted circle (combining mark)
         return true;
     }
