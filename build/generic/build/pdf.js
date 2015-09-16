76fa46ce6f628d9b33a5ca2e5b10a1a4e84ac853
Damien Guérin <dguerin@sqli.com>
2015-09-16 16:55:53 +0200

    Change styles

--- build/generic/build/pdf.js
+++ build/generic/build/pdf.js
@@ -22,8 +22,8 @@ if (typeof PDFJS === 'undefined') {
   (typeof window !== 'undefined' ? window : this).PDFJS = {};
 }
 
-PDFJS.version = '1.1.339';
-PDFJS.build = '3bc63b4';
+PDFJS.version = '1.1.340';
+PDFJS.build = 'b471308';
 
 (function pdfjsWrapper() {
   // Use strict in our context only - users might not want it
