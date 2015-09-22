4ba5f01b361985e27f7728f12b11ac7a5c4a3931
Damien Guérin <dguerin@sqli.com>
2015-09-22 17:41:34 +0200

    Remove Open File

--- build/generic/build/pdf.js
+++ build/generic/build/pdf.js
@@ -22,8 +22,8 @@ if (typeof PDFJS === 'undefined') {
   (typeof window !== 'undefined' ? window : this).PDFJS = {};
 }
 
-PDFJS.version = '1.1.342';
-PDFJS.build = '47120e6';
+PDFJS.version = '1.1.343';
+PDFJS.build = 'f647403';
 
 (function pdfjsWrapper() {
   // Use strict in our context only - users might not want it
