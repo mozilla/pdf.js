8dff301ce19f2298f3e48fa187b2a62e1bf3af33
Yury Delendik <ydelendik@mozilla.com>
2015-11-25 18:21:23 -0600

    Worker shall wait for MessageHandler to be created at api side.

--- src/display/api.js
+++ src/display/api.js
@@ -359,6 +359,7 @@ PDFJS.getDocument = function getDocument(src,
         throw new Error('Loading aborted');
       }
       var messageHandler = new MessageHandler(docId, workerId, worker.port);
+      messageHandler.send('Ready', null);
       var transport = new WorkerTransport(messageHandler, task, rangeTransport);
       task._transport = transport;
     });
