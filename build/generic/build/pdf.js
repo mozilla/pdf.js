dbd2ffaf6b7032058d800a4b8b1bb9bb7caba2ea
Stijn Deschuymer <stijn.deschuymer@showpad.com>
2015-12-21 15:23:56 +0100

    build

--- build/generic/build/pdf.js
+++ build/generic/build/pdf.js
@@ -20,8 +20,8 @@ if (typeof PDFJS === 'undefined') {
   (typeof window !== 'undefined' ? window : this).PDFJS = {};
 }
 
-PDFJS.version = '1.3.119';
-PDFJS.build = 'f849e4c';
+PDFJS.version = '1.3.123';
+PDFJS.build = '9363c44';
 
 (function pdfjsWrapper() {
   // Use strict in our context only - users might not want it
