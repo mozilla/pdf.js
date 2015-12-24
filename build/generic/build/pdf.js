b60bd9cf8af578f94596e249bf1d35cde5697099
Stijn Deschuymer <stijn.deschuymer@showpad.com>
2015-12-24 13:07:22 +0100

    minor css change, selection color

--- build/generic/build/pdf.js
+++ build/generic/build/pdf.js
@@ -20,8 +20,8 @@ if (typeof PDFJS === 'undefined') {
   (typeof window !== 'undefined' ? window : this).PDFJS = {};
 }
 
-PDFJS.version = '1.3.126';
-PDFJS.build = '39f0832';
+PDFJS.version = '1.3.127';
+PDFJS.build = '651e46a';
 
 (function pdfjsWrapper() {
   // Use strict in our context only - users might not want it
