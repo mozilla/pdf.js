55036b88cf0467c3e5fda20bb4267d762da4ab19
Huynh Hiep Si <hhiepsi@gmail.com>
2016-01-15 14:06:53 +0700

    comments

--- src/core/image.js
+++ src/core/image.js
@@ -570,13 +570,6 @@ var PDFImage = (function PDFImageClosure() {
       imgData.height = newHeight;
     },
 
-    /**
-     * Resize large resolution PDFS as to improve rendering time
-     * @param {Uint8Array} imgData image data.
-     * @param {Number} comps Number of color components, 1 or 3 is supported.
-     * @param {Number} bpc
-     * @return {TypedArray} Resized image data.
-     */
     resizeGrayPixels: function(imgData, comps, bpc) {
       var scaleBits;
       var h1 = imgData.height;
@@ -715,6 +708,7 @@ var PDFImage = (function PDFImageClosure() {
         // Not resize data when rendering on web
         if (!this.print) {
           var needResize = this.shallResizeImage(numComps, bpc);
+
           if (needResize) {
             this.resizeGrayPixels(resizedImgData, numComps, bpc)
 
