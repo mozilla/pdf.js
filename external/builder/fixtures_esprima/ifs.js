47ae9e245b577d423791a34efbad001bfcecc49a
Yury Delendik <ydelendik@mozilla.com>
2016-05-10 18:05:29 -0500

    Adds esprima-based preprocessor.

---
+++ external/builder/fixtures_esprima/ifs.js
@@ -0,0 +1,25 @@
+if ('test') {
+  "1";
+}
+if (true) {
+  "1";
+}
+if (true) {
+  "1";
+} else {
+  "2";
+}
+if (false) {
+  "1";
+}
+if (false) {
+  "1";
+} else {
+  "2";
+}
+if (true && false) {
+  "1";
+}
+if (true && false || '1') {
+  "1";
+}
