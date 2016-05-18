734d65df6a1d35e84ea306fcccb76da21d8588ac
Arnaud <arnaudschlupp@Arnauds-MacBook-Pro.local>
2016-05-18 17:28:23 +0200

    generic build

--- build/generic/build/pdf.js
+++ build/generic/build/pdf.js
@@ -20,8 +20,8 @@ if (typeof PDFJS === 'undefined') {
   (typeof window !== 'undefined' ? window : this).PDFJS = {};
 }
 
-PDFJS.version = '1.3.137';
-PDFJS.build = '9235c2c';
+PDFJS.version = '1.3.141';
+PDFJS.build = '7a428fc';
 
 (function pdfjsWrapper() {
   // Use strict in our context only - users might not want it
