3096267b08cffd63dfcbefeeaba32d86fd32ed6b
Dennis John <dennis.r.john@gmail.com>
2016-12-22 09:34:53 -0700

    When exporting pdf to canvas, can pass a backgroundColor parameter (rgb or rgba) to set the canvas background color to something other than white

--- src/display/api.js
+++ src/display/api.js
@@ -2067,7 +2067,8 @@ var InternalRenderTask = (function InternalRenderTaskClosure() {
       this.gfx = new CanvasGraphics(params.canvasContext, this.commonObjs,
                                     this.objs, params.imageLayer);
 
-      this.gfx.beginDrawing(params.transform, params.viewport, transparency);
+      this.gfx.beginDrawing(params.transform, params.viewport,
+                                transparency, params.background);
       this.operatorListIdx = 0;
       this.graphicsReady = true;
       if (this.graphicsReadyCallback) {
