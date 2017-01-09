6265bb60384f839b93840724b67d9b4cef3261fa
Yury Delendik <ydelendik@mozilla.com>
2017-01-09 13:15:09 -0600

    Fixes preprocessor testing and adds deadcode removal.

--- external/builder/fixtures_esprima/comments-expected.js
+++ external/builder/fixtures_esprima/comments-expected.js
@@ -1,28 +1,28 @@
 function f1() {
-  /* head */
-  "1";
-  /* mid */
-  "2";
+ /* head */
+ "1";
+ /* mid */
+ "2";
 }
 /* tail */
 function f2() {
-  // head
-  "1";
-  // mid
-  "2";
+ // head
+ "1";
+ // mid
+ "2";
 }
 // tail
 function f2() {
-  if ("1") {
-    // begin block
-    "1";
+ if ("1") {
+  // begin block
+  "1";
+ }
+ "2";
+ // trailing
+ if (/* s */
+  "3")
+  /*e*/
+  {
+   "4";
   }
-  "2";
-  // trailing
-  if (/* s */
-    "3")
-    /*e*/
-    {
-      "4";
-    }
 }
