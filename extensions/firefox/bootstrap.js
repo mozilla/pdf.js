a5d5b970afa16745044b41f9e31c96d06aac36e5
Jonas Jenwald <jonas.jenwald@gmail.com>
2017-01-23 23:41:56 +0100

    [Firefox addon] Enforce double quotes, using ESLint, to avoid linting errors in mozilla-central (issue 7957)
    Given that this patch causes a lot of churn in the addon code, I wouldn't really mind if we ultimately decide against doing this and just add a rule exception in mozilla-central instead.[1]
    
    ---
    [1] Note that I used the ESLint `--fix` option, hence writing this commit message actually took longer time than the creation of the patch :-)

--- extensions/firefox/bootstrap.js
+++ extensions/firefox/bootstrap.js
@@ -15,10 +15,10 @@
 /* globals Components, Services, dump, XPCOMUtils, PdfStreamConverter,
            APP_SHUTDOWN, PdfjsChromeUtils, PdfjsContentUtils */
 
-'use strict';
+"use strict";
 
-const RESOURCE_NAME = 'pdf.js';
-const EXT_PREFIX = 'extensions.uriloader@pdf.js';
+const RESOURCE_NAME = "pdf.js";
+const EXT_PREFIX = "extensions.uriloader@pdf.js";
 
 const Cc = Components.classes;
 const Ci = Components.interfaces;
@@ -26,8 +26,8 @@ const Cm = Components.manager;
 const Cu = Components.utils;
 const Cr = Components.results;
 
-Cu.import('resource://gre/modules/XPCOMUtils.jsm');
-Cu.import('resource://gre/modules/Services.jsm');
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
 
 function getBoolPref(pref, def) {
   try {
@@ -38,31 +38,31 @@ function getBoolPref(pref, def) {
 }
 
 function log(str) {
-  if (!getBoolPref(EXT_PREFIX + '.pdfBugEnabled', false)) {
+  if (!getBoolPref(EXT_PREFIX + ".pdfBugEnabled", false)) {
     return;
   }
-  dump(str + '\n');
+  dump(str + "\n");
 }
 
 function initializeDefaultPreferences() {
   var DEFAULT_PREFERENCES =
 //#include ../../web/default_preferences.json
 //#if false
-    'end of DEFAULT_PREFERENCES';
+    "end of DEFAULT_PREFERENCES";
 //#endif
 
-  var defaultBranch = Services.prefs.getDefaultBranch(EXT_PREFIX + '.');
+  var defaultBranch = Services.prefs.getDefaultBranch(EXT_PREFIX + ".");
   var defaultValue;
   for (var key in DEFAULT_PREFERENCES) {
     defaultValue = DEFAULT_PREFERENCES[key];
     switch (typeof defaultValue) {
-      case 'boolean':
+      case "boolean":
         defaultBranch.setBoolPref(key, defaultValue);
         break;
-      case 'number':
+      case "number":
         defaultBranch.setIntPref(key, defaultValue);
         break;
-      case 'string':
+      case "string":
         defaultBranch.setCharPref(key, defaultValue);
         break;
     }
@@ -126,27 +126,27 @@ var e10sEnabled = false;
 function startup(aData, aReason) {
   // Setup the resource url.
   var ioService = Services.io;
-  var resProt = ioService.getProtocolHandler('resource')
+  var resProt = ioService.getProtocolHandler("resource")
                   .QueryInterface(Ci.nsIResProtocolHandler);
-  var aliasURI = ioService.newURI('content/', 'UTF-8', aData.resourceURI);
+  var aliasURI = ioService.newURI("content/", "UTF-8", aData.resourceURI);
   resProt.setSubstitution(RESOURCE_NAME, aliasURI);
 
   pdfBaseUrl = aData.resourceURI.spec;
 
-  Cu.import(pdfBaseUrl + 'content/PdfjsChromeUtils.jsm');
+  Cu.import(pdfBaseUrl + "content/PdfjsChromeUtils.jsm");
   PdfjsChromeUtils.init();
-  Cu.import(pdfBaseUrl + 'content/PdfjsContentUtils.jsm');
+  Cu.import(pdfBaseUrl + "content/PdfjsContentUtils.jsm");
   PdfjsContentUtils.init();
 
   // Load the component and register it.
-  var pdfStreamConverterUrl = pdfBaseUrl + 'content/PdfStreamConverter.jsm';
+  var pdfStreamConverterUrl = pdfBaseUrl + "content/PdfStreamConverter.jsm";
   Cu.import(pdfStreamConverterUrl);
   pdfStreamConverterFactory.register(PdfStreamConverter);
 
   try {
-    let globalMM = Cc['@mozilla.org/globalmessagemanager;1']
+    let globalMM = Cc["@mozilla.org/globalmessagemanager;1"]
                      .getService(Ci.nsIFrameScriptLoader);
-    globalMM.loadFrameScript('chrome://pdf.js/content/content.js', true);
+    globalMM.loadFrameScript("chrome://pdf.js/content/content.js", true);
     e10sEnabled = true;
   } catch (ex) {
   }
@@ -160,32 +160,32 @@ function shutdown(aData, aReason) {
   }
 
   if (e10sEnabled) {
-    let globalMM = Cc['@mozilla.org/globalmessagemanager;1']
+    let globalMM = Cc["@mozilla.org/globalmessagemanager;1"]
                      .getService(Ci.nsIMessageBroadcaster);
-    globalMM.broadcastAsyncMessage('PDFJS:Child:shutdown');
-    globalMM.removeDelayedFrameScript('chrome://pdf.js/content/content.js');
+    globalMM.broadcastAsyncMessage("PDFJS:Child:shutdown");
+    globalMM.removeDelayedFrameScript("chrome://pdf.js/content/content.js");
   }
 
   var ioService = Services.io;
-  var resProt = ioService.getProtocolHandler('resource')
+  var resProt = ioService.getProtocolHandler("resource")
                   .QueryInterface(Ci.nsIResProtocolHandler);
   // Remove the resource url.
   resProt.setSubstitution(RESOURCE_NAME, null);
   // Remove the contract/component.
   pdfStreamConverterFactory.unregister();
   // Unload the converter
-  var pdfStreamConverterUrl = pdfBaseUrl + 'content/PdfStreamConverter.jsm';
+  var pdfStreamConverterUrl = pdfBaseUrl + "content/PdfStreamConverter.jsm";
   Cu.unload(pdfStreamConverterUrl);
 
   PdfjsContentUtils.uninit();
-  Cu.unload(pdfBaseUrl + 'content/PdfjsContentUtils.jsm');
+  Cu.unload(pdfBaseUrl + "content/PdfjsContentUtils.jsm");
   PdfjsChromeUtils.uninit();
-  Cu.unload(pdfBaseUrl + 'content/PdfjsChromeUtils.jsm');
+  Cu.unload(pdfBaseUrl + "content/PdfjsChromeUtils.jsm");
 }
 
 function install(aData, aReason) {
   // TODO remove after some time -- cleanup of unused preferences
-  Services.prefs.clearUserPref(EXT_PREFIX + '.database');
+  Services.prefs.clearUserPref(EXT_PREFIX + ".database");
 }
 
 function uninstall(aData, aReason) {
--- extensions/firefox/tools/l10n.js
+++ extensions/firefox/tools/l10n.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
 // Small subset of the webL10n API by Fabien Cazenave for pdf.js extension.
 (function(window) {
-  var gLanguage = '';
+  var gLanguage = "";
   var gExternalLocalizerServices = null;
-  var gReadyState = 'loading';
+  var gReadyState = "loading";
 
   // fetch an l10n objects
   function getL10nData(key) {
     var response = gExternalLocalizerServices.getStrings(key);
     var data = JSON.parse(response);
     if (!data) {
-      console.warn('[l10n] #' + key + ' missing for [' + gLanguage + ']');
+      console.warn("[l10n] #" + key + " missing for [" + gLanguage + "]");
     }
     return data;
   }
@@ -22,25 +22,25 @@
       return text;
     }
     return text.replace(/\{\{\s*(\w+)\s*\}\}/g, function(all, name) {
-      return (name in args ? args[name] : '{{' + name + '}}');
+      return (name in args ? args[name] : "{{" + name + "}}");
     });
   }
 
   // translate a string
   function translateString(key, args, fallback) {
-    var i = key.lastIndexOf('.');
+    var i = key.lastIndexOf(".");
     var name, property;
     if (i >= 0) {
       name = key.substring(0, i);
       property = key.substring(i + 1);
     } else {
       name = key;
-      property = 'textContent';
+      property = "textContent";
     }
     var data = getL10nData(name);
     var value = (data && data[property]) || fallback;
     if (!value) {
-      return '{{' + key + '}}';
+      return "{{" + key + "}}";
     }
     return substArguments(value, args);
   }
@@ -65,7 +65,7 @@
       try {
         args = JSON.parse(element.dataset.l10nArgs);
       } catch (e) {
-        console.warn('[l10n] could not parse arguments for #' + key + '');
+        console.warn("[l10n] could not parse arguments for #" + key + "");
       }
     }
 
@@ -79,10 +79,10 @@
 
   // translate an HTML subtree
   function translateFragment(element) {
-    element = element || document.querySelector('html');
+    element = element || document.querySelector("html");
 
     // check all translatable children (= w/ a `data-l10n-id' attribute)
-    var children = element.querySelectorAll('*[data-l10n-id]');
+    var children = element.querySelectorAll("*[data-l10n-id]");
     var elementCount = children.length;
     for (var i = 0; i < elementCount; i++) {
       translateElement(children[i]);
@@ -99,16 +99,16 @@
 
     translateFragment();
 
-    gReadyState = 'complete';
+    gReadyState = "complete";
 
     // fire a 'localized' DOM event
-    var evtObject = document.createEvent('Event');
-    evtObject.initEvent('localized', false, false);
+    var evtObject = document.createEvent("Event");
+    evtObject.initEvent("localized", false, false);
     evtObject.language = gLanguage;
     window.dispatchEvent(evtObject);
   }
 
-  window.addEventListener('DOMContentLoaded', function() {
+  window.addEventListener("DOMContentLoaded", function() {
     if (gExternalLocalizerServices) {
       translateDocument();
     }
@@ -129,12 +129,12 @@
     getDirection() {
       // http://www.w3.org/International/questions/qa-scripts
       // Arabic, Hebrew, Farsi, Pashto, Urdu
-      var rtlList = ['ar', 'he', 'fa', 'ps', 'ur'];
+      var rtlList = ["ar", "he", "fa", "ps", "ur"];
 
       // use the short language code for "full" codes like 'ar-sa' (issue 5440)
-      var shortCode = gLanguage.split('-')[0];
+      var shortCode = gLanguage.split("-")[0];
 
-      return (rtlList.indexOf(shortCode) >= 0) ? 'rtl' : 'ltr';
+      return (rtlList.indexOf(shortCode) >= 0) ? "rtl" : "ltr";
     },
 
     getReadyState() {
@@ -145,8 +145,8 @@
       gExternalLocalizerServices = externalLocalizerServices;
 
       // ... in case if we missed DOMContentLoaded above.
-      if (window.document.readyState === 'interactive' ||
-          window.document.readyState === 'complete') {
+      if (window.document.readyState === "interactive" ||
+          window.document.readyState === "complete") {
         translateDocument();
       }
     },
