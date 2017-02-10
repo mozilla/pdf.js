63f13773e717d2209c1311f929a1b2ffd5396e60
Jonas Jenwald <jonas.jenwald@gmail.com>
2017-02-10 14:19:26 +0100

    Use `ChildNode.remove` instead of `ChildNode.ParentNode.removeChild` in a couple of places (bug 1334831, issue 8008)
    Re: [bug 1334831](https://bugzilla.mozilla.org/show_bug.cgi?id=1334831) and issue 8008.
    
    Note that according to the specification, see https://dom.spec.whatwg.org/#interface-childnode, the `remove` method shouldn't throw.
    This is also consistent with e.g. the Firefox implementation, see http://searchfox.org/mozilla-central/rev/d3307f19d5dac31d7d36fc206b00b686de82eee4/dom/base/nsINode.cpp#1852.
    
    Obviously this isn't supported in IE (because that would be too easy), however we can easily polyfill it to avoid having to WONTFIX the bug/issue.

--- web/grab_to_pan.js
+++ web/grab_to_pan.js
@@ -184,9 +184,8 @@
       this.element.removeEventListener('scroll', this._endPan, true);
       this.document.removeEventListener('mousemove', this._onmousemove, true);
       this.document.removeEventListener('mouseup', this._endPan, true);
-      if (this.overlay.parentNode) {
-        this.overlay.parentNode.removeChild(this.overlay);
-      }
+      // Note: ChildNode.remove doesn't throw if the parentNode is undefined.
+      this.overlay.remove();
     }
   };
 
