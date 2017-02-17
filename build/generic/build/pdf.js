8d4a7c00be367bc0ff66b88b0862ed2423708768
Daniel Johnson <danielj@ifixit.com>
2017-02-17 14:09:56 -0700

    commit build files

--- build/generic/build/pdf.js
+++ build/generic/build/pdf.js
@@ -28,8 +28,8 @@ factory((root.pdfjsDistBuildPdf = {}));
   // Use strict in our context only - users might not want it
   'use strict';
 
-var pdfjsVersion = '1.6.236';
-var pdfjsBuild = '21543f4';
+var pdfjsVersion = '1.6.240';
+var pdfjsBuild = '65d157e';
 
   var pdfjsFilePath =
     typeof document !== 'undefined' && document.currentScript ?
