08e15c963d59f8e6c52e745635a9675abb285580
Cole Bemis <cole@ifixit.com>
2017-03-27 11:55:42 -0700

    Commit build files

--- build/generic/build/pdf.worker.js
+++ build/generic/build/pdf.worker.js
@@ -28,8 +28,8 @@ factory((root.pdfjsDistBuildPdfWorker = {}));
   // Use strict in our context only - users might not want it
   'use strict';
 
-var pdfjsVersion = '1.6.307';
-var pdfjsBuild = '171021c';
+var pdfjsVersion = '1.6.311';
+var pdfjsBuild = '56377a4';
 
   var pdfjsFilePath =
     typeof document !== 'undefined' && document.currentScript ?
