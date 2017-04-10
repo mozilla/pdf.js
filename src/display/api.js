3b027df271a18659f9d871f772b8547d78ec383a
tomascorralcasas <amischol@gmail.com>
2017-04-10 09:01:44 +0200

    Fix worker resolution on using minified version
    - When the minified version is used the resolver of the worker can not find it properly and throws 404 error.
    - The problem was that:
      - It was getting the current name of the file.
      - It was replacing **.js** by **.worker.js**
    - When it was loading the unminified version it was working fine because:
      - *pdf.js - .js + .worker.js*  = **pdf.worker.js**
    - When it was loading the minified version it didtn't work because:
      - *pdf.min.js - .js + .worker.js* = **pdf.min.worker.js**
      - **pdf.min.worker.js** doesn't exist the real file name is **pdf.worker.min.js**

--- src/display/api.js
+++ src/display/api.js
@@ -1097,7 +1097,7 @@ var PDFWorker = (function PDFWorkerClosure() {
     if (typeof PDFJSDev !== 'undefined' &&
         PDFJSDev.test('PRODUCTION && !(MOZCENTRAL || FIREFOX)') &&
         pdfjsFilePath) {
-      return pdfjsFilePath.replace(/(\.(?:min\.)?js)$/i, '.worker$');
+      return pdfjsFilePath.replace(/(\.(?:min\.)?js)$/i, '.worker$1');
     }
     error('No PDFJS.workerSrc specified');
   }
