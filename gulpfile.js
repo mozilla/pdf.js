52a2da7633adf8074b47beae0d6bee614c3e5832
Mukul Mishra <mkmishra1895@gmail.com>
2017-04-13 20:46:52 +0530

    Adds streams-lib polyfill and exports ReadableStream from shared/util.
    Added test for ReadableStream.
    
    Adds ref-implementation license-header in streams-lib
    and change gulp task to copy external/streams/ in build/
    external/streams/ and build/dist/external/streams folder.
    
    Adds README.md and LICENSE.md

--- gulpfile.js
+++ gulpfile.js
@@ -1012,8 +1012,7 @@ gulp.task('lib', ['buildnumber'], function () {
   };
   var licenseHeader = fs.readFileSync('./src/license_header.js').toString();
   var preprocessor2 = require('./external/builder/preprocessor2.js');
-
-  return merge([
+  var buildLib = merge([
     gulp.src([
       'src/{core,display}/*.js',
       'src/shared/{compatibility,util}.js',
@@ -1027,6 +1026,11 @@ gulp.task('lib', ['buildnumber'], function () {
     gulp.src('test/unit/*.js', {base: '.'}),
   ]).pipe(transform(preprocess))
     .pipe(gulp.dest('build/lib/'));
+  return merge([
+    buildLib,
+    gulp.src('external/streams/streams-lib.js', {base: '.'})
+      .pipe(gulp.dest('build/')),
+  ]);
 });
 
 gulp.task('web-pre', ['generic', 'extension', 'jsdoc']);
@@ -1320,6 +1324,8 @@ gulp.task('dist-repo-prepare', ['dist-pre'], function () {
     createStringSource('bower.json', JSON.stringify(bowerManifest, null, 2));
 
   return merge([
+    gulp.src('external/streams/streams-lib.js', {base: '.'})
+      .pipe(gulp.dest('build/dist/')),
     packageJsonSrc.pipe(gulp.dest(DIST_DIR)),
     bowerJsonSrc.pipe(gulp.dest(DIST_DIR)),
     vinyl.src('external/dist/**/*',
