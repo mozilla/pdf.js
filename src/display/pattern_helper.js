8dea9488199f7324ac4427393cb8ecde2c6d6fb2
Jonas Jenwald <jonas.jenwald@gmail.com>
2017-05-24 12:12:14 +0200

    Ensure that `TilingPattern`s have valid (non-zero) /BBox arrays (issue 8330)
    Fixes 8330.

--- src/display/pattern_helper.js
+++ src/display/pattern_helper.js
@@ -304,7 +304,7 @@ var TilingPattern = (function TilingPatternClosure() {
   function TilingPattern(IR, color, ctx, canvasGraphicsFactory, baseTransform) {
     this.operatorList = IR[2];
     this.matrix = IR[3] || [1, 0, 0, 1, 0, 0];
-    this.bbox = Util.normalizeRect(IR[4]);
+    this.bbox = IR[4];
     this.xstep = IR[5];
     this.ystep = IR[6];
     this.paintType = IR[7];
