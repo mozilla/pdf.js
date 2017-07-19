64bb1a84ddb719e912c0a6eab4c9a1d998bc38dd
Audun Halland <audun.halland@gmail.com>
2017-07-19 13:42:11 +0200

    Split text content items upon word/char spacing operator.

--- src/core/evaluator.js
+++ src/core/evaluator.js
@@ -1545,9 +1545,11 @@ var PartialEvaluator = (function PartialEvaluatorClosure() {
                 args[4], args[5]);
               break;
             case OPS.setCharSpacing:
+              flushTextContentItem();
               textState.charSpacing = args[0];
               break;
             case OPS.setWordSpacing:
+              flushTextContentItem();
               textState.wordSpacing = args[0];
               break;
             case OPS.beginText:
