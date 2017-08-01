23daafd7289a779d2e8ffc25315f769d285dfb12
Jonas Jenwald <jonas.jenwald@gmail.com>
2017-08-01 14:13:49 +0200

    Implement a `PDFSinglePageViewer` class (issue 8188)
    The new `PDFSinglePageViewer` class extends the previously created abstract `BaseViewer` class.
    
    There's *a lot* of existing functionality in `PDFViewer` that depends on all the pages being loaded and synchronously available, once the `setDocument` method has been called.
    Given that initializing `PDFPageView` instances requires passing a DOM element to which the page is attached, the simplest solution I could come up with is to append all pages to a (hidden) document fragment and just swap them (one at a time) into the viewer when page switching occurs.

--- web/pdf_viewer.component.js
+++ web/pdf_viewer.component.js
@@ -17,6 +17,7 @@
 
 var pdfjsLib = require('./pdfjs.js');
 var pdfjsWebPDFViewer = require('./pdf_viewer.js');
+var pdfjsWebPDFSinglePageViewer = require('./pdf_single_page_viewer');
 var pdfjsWebPDFPageView = require('./pdf_page_view.js');
 var pdfjsWebPDFLinkService = require('./pdf_link_service.js');
 var pdfjsWebTextLayerBuilder = require('./text_layer_builder.js');
@@ -30,6 +31,7 @@ var pdfjsWebGenericL10n = require('./genericl10n.js');
 var PDFJS = pdfjsLib.PDFJS;
 
 PDFJS.PDFViewer = pdfjsWebPDFViewer.PDFViewer;
+PDFJS.PDFSinglePageViewer = pdfjsWebPDFSinglePageViewer.PDFSinglePageViewer;
 PDFJS.PDFPageView = pdfjsWebPDFPageView.PDFPageView;
 PDFJS.PDFLinkService = pdfjsWebPDFLinkService.PDFLinkService;
 PDFJS.SimpleLinkService = pdfjsWebPDFLinkService.SimpleLinkService;
