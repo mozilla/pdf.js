f187187f1f8eeebf16da1a17d738a46c9cfd9112
Waleed Ashraf <waleedashraf@outlook.com>
2017-11-16 14:47:34 +0500

    add padding-left to pageNmber in mobile view

--- build/generic/build/pdf.worker.js
+++ build/generic/build/pdf.worker.js
@@ -28,8 +28,8 @@ factory((root.pdfjsDistBuildPdfWorker = {}));
   // Use strict in our context only - users might not want it
   'use strict';
 
-var pdfjsVersion = '1.5.207';
-var pdfjsBuild = '39b62559';
+var pdfjsVersion = '1.5.208';
+var pdfjsBuild = '2ec50d8e';
 
   var pdfjsFilePath =
     typeof document !== 'undefined' && document.currentScript ?
