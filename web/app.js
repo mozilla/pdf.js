6a8fd95fa644b8519b4f7aac775d743e96b2101e
AlexejStukov <sergej.morokin@googlemail.com>
2018-02-06 09:15:59 +0100

    locally hosted viewer should allow any file location
    Included 'qrc://' for Qt resource files

--- web/app.js
+++ web/app.js
@@ -1482,7 +1482,7 @@ let PDFViewerApplication = {
 
 let validateFileURL;
 if (typeof PDFJSDev === 'undefined' || PDFJSDev.test('GENERIC')) {
-  const HOSTED_VIEWER_ORIGINS = ['null',
+  const HOSTED_VIEWER_ORIGINS = ['null', 'file://', 'qrc://'
     'http://mozilla.github.io', 'https://mozilla.github.io'];
   validateFileURL = function validateFileURL(file) {
     if (file === undefined) {
