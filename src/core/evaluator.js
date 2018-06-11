1cf04aa765b2fe1871c44bc4df883d18379aaed4
Jonas Jenwald <jonas.jenwald@gmail.com>
2018-06-11 17:25:46 +0200

    Improve the warning messages printed by `PartialEvaluator.{getOperatorList, getTextContent} when errors are being ignored
    Currently the actual errors aren't printed, which can make debugging harder than necessary.

--- src/core/evaluator.js
+++ src/core/evaluator.js
@@ -1261,7 +1261,8 @@ var PartialEvaluator = (function PartialEvaluatorClosure() {
           // notification and allow rendering to continue.
           this.handler.send('UnsupportedFeature',
                             { featureId: UNSUPPORTED_FEATURES.unknown, });
-          warn('getOperatorList - ignoring errors during task: ' + task.name);
+          warn(`getOperatorList - ignoring errors during "${task.name}" ` +
+               `task: "${reason}".`);
 
           closePendingRestoreOPS();
           return;
@@ -1846,7 +1847,8 @@ var PartialEvaluator = (function PartialEvaluatorClosure() {
         }
         if (this.options.ignoreErrors) {
           // Error(s) in the TextContent -- allow text-extraction to continue.
-          warn('getTextContent - ignoring errors during task: ' + task.name);
+          warn(`getTextContent - ignoring errors during "${task.name}" ` +
+               `task: "${reason}".`);
 
           flushTextContentItem();
           enqueueChunk();
