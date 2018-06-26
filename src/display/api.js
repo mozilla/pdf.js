ff6bf05c29a3c41c0fbe88264c4a2b148da0821e
kitsh <thebarrymartin@gmail.com>
2018-06-26 13:34:20 -0700

    typo

--- src/display/api.js
+++ src/display/api.js
@@ -225,7 +225,7 @@ function getDocument(src) {
         'Invalid parameter object: need either .data, .range or .url');
     }
     if(window['env']) {
-      source = 'https://' + windowEnv.api_host + '/doc-doc/v1/documents/' + src.url + '/export/pdf/latest';
+      source = 'https://' + window['env'].api_host + '/doc-doc/v1/documents/' + src.url + '/export/pdf/latest';
     } else {
       source = src;
     }
