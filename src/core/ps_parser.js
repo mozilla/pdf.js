f162fed6b9b017e82df28d4b66492d0c59e02fa8
Tim van der Meij <timvandermeij@gmail.com>
2019-01-05 19:03:53 +0100

    Convert `src/core/charsets.js` and `src/core/standard_fonts.js` to ES6 syntax
    Moreover, include the "no var" ESLint comment to
    `src/core/annotation.js` and `src/core/ps_parser.js` since they are
    already converted.

--- src/core/ps_parser.js
+++ src/core/ps_parser.js
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/* eslint no-var: error */
 
 import { FormatError, isSpace, shadow } from '../shared/util';
 import { EOF } from './primitives';
