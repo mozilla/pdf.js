09ee98b44b65ba5f74400ddf2d542ef89843a668
Luís Takahashi <takahashihideki408@gmail.com>
2019-10-10 18:25:41 -0300

    Add Create React App example with TypeScript and basic usage

---
+++ examples/create-react-app/src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Pdf from './pdf';
+
+export default function App () {
+  return (
+    <main>
+      <h1>PDF.js rendering example with Create React App</h1>
+      <Pdf />
+    </main>
+  )
+}
---
+++ examples/create-react-app/src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
