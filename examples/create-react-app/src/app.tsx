d26fab9987fe015bbed2a9459dddae041b7892a8
Luis <takahashihideki408@gmail.com>
2019-10-10 10:43:21 -0300

    Changed app name

--- examples/create-react-app/src/app.tsx
+++ examples/create-react-app/src/app.tsx
@@ -5,7 +5,7 @@ import Pdf from './pdf';
 export default function App () {
   return (
     <main>
-      <h1>Simple pdf render example</h1>
+      <h1>PDF.js rendering example with Create React App</h1>
       <Pdf />
     </main>
   )
