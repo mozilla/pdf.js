5640aaca17f007a1a433dd9060b948183860dc03
Jonas Jenwald <jonas.jenwald@gmail.com>
2020-01-08 13:57:31 +0100

    Remove the `FIREFOX` build flag, since it's completely unused
    After PR 9566, which removed all of the old Firefox extension code, the `FIREFOX` build flag is no longer used for anything.
    It thus seems to me that it should be removed, for a couple of reasons:
     - It's simply dead code now, which only serves to add confusion when looking at the `PDFJSDev` calls.
     - It used to be that `MOZCENTRAL` and `FIREFOX` was *almost* always used together. However, ever since PR 9566 there's obviously been no effort put into keeping the `FIREFOX` build flags up to date.
     - In the event that a new, Webextension based, Firefox addon is created in the future you'd still need to audit all `MOZCENTRAL` (and possibly `CHROME`) build flags to see what'd make sense for the addon.

--- web/app.js
+++ web/app.js
@@ -578,7 +578,7 @@ const PDFViewerApplication = {
   initPassiveLoading() {
     if (
       typeof PDFJSDev === "undefined" ||
-      !PDFJSDev.test("FIREFOX || MOZCENTRAL || CHROME")
+      !PDFJSDev.test("MOZCENTRAL || CHROME")
     ) {
       throw new Error("Not implemented: initPassiveLoading");
     }
@@ -733,7 +733,7 @@ const PDFViewerApplication = {
           value = document.URL.split("#")[0];
         } else if (
           typeof PDFJSDev !== "undefined" &&
-          PDFJSDev.test("FIREFOX || MOZCENTRAL || CHROME")
+          PDFJSDev.test("MOZCENTRAL || CHROME")
         ) {
           value = this.baseUrl;
         }
@@ -927,10 +927,7 @@ const PDFViewerApplication = {
       }
     }
 
-    if (
-      typeof PDFJSDev === "undefined" ||
-      !PDFJSDev.test("FIREFOX || MOZCENTRAL")
-    ) {
+    if (typeof PDFJSDev === "undefined" || !PDFJSDev.test("MOZCENTRAL")) {
       const errorWrapperConfig = this.appConfig.errorWrapper;
       const errorWrapper = errorWrapperConfig.container;
       errorWrapper.removeAttribute("hidden");
@@ -1045,7 +1042,7 @@ const PDFViewerApplication = {
     let baseDocumentUrl;
     if (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) {
       baseDocumentUrl = null;
-    } else if (PDFJSDev.test("FIREFOX || MOZCENTRAL")) {
+    } else if (PDFJSDev.test("MOZCENTRAL")) {
       baseDocumentUrl = this.baseUrl;
     } else if (PDFJSDev.test("CHROME")) {
       baseDocumentUrl = location.href.split("#")[0];
@@ -1809,7 +1806,7 @@ function webViewerInitialized() {
     const params = parseQueryString(queryString);
     file = "file" in params ? params.file : AppOptions.get("defaultUrl");
     validateFileURL(file);
-  } else if (PDFJSDev.test("FIREFOX || MOZCENTRAL")) {
+  } else if (PDFJSDev.test("MOZCENTRAL")) {
     file = window.location.href.split("#")[0];
   } else if (PDFJSDev.test("CHROME")) {
     file = AppOptions.get("defaultUrl");
@@ -1943,7 +1940,7 @@ if (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) {
       PDFViewerApplication.open(file);
     }
   };
-} else if (PDFJSDev.test("FIREFOX || MOZCENTRAL || CHROME")) {
+} else if (PDFJSDev.test("MOZCENTRAL || CHROME")) {
   webViewerOpenFileViaURL = function webViewerOpenFileViaURL(file) {
     PDFViewerApplication.setTitleUsingUrl(file);
     PDFViewerApplication.initPassiveLoading();
@@ -2521,10 +2518,7 @@ function webViewerKeyDown(evt) {
     }
   }
 
-  if (
-    typeof PDFJSDev === "undefined" ||
-    !PDFJSDev.test("FIREFOX || MOZCENTRAL")
-  ) {
+  if (typeof PDFJSDev === "undefined" || !PDFJSDev.test("MOZCENTRAL")) {
     // CTRL or META without shift
     if (cmd === 1 || cmd === 8) {
       switch (evt.keyCode) {
