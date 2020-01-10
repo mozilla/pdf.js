43d4788b9583edfe2d02260e22dddad686f6e95d
Jonas Jenwald <jonas.jenwald@gmail.com>
2020-01-10 14:35:12 +0100

    Add support for `Promise.allSettled`
    Please see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/allSettled

--- src/shared/message_handler.js
+++ src/shared/message_handler.js
@@ -551,13 +551,13 @@ class MessageHandler {
   async _deleteStreamController(streamId) {
     // Delete the `streamController` only when the start, pull, and cancel
     // capabilities have settled, to prevent `TypeError`s.
-    await Promise.all(
+    await Promise.allSettled(
       [
         this.streamControllers[streamId].startCall,
         this.streamControllers[streamId].pullCall,
         this.streamControllers[streamId].cancelCall,
       ].map(function(capability) {
-        return capability && capability.promise.catch(function() {});
+        return capability && capability.promise;
       })
     );
     delete this.streamControllers[streamId];
