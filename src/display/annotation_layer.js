66871c4436947d63b87eb4661896ea25e52bfe6b
roccobeno <62256813+roccobeno@users.noreply.github.com>
2020-03-18 18:07:42 +0100

    add a name attribute on input and select element

--- src/display/annotation_layer.js
+++ src/display/annotation_layer.js
@@ -455,6 +455,7 @@ class TextWidgetAnnotationElement extends WidgetAnnotationElement {
         element.setAttribute("value", this.data.fieldValue);
       }
 
+      element.setAttribute("name", this.data.fieldName);
       element.disabled = this.data.readOnly;
 
       if (this.data.maxLen !== null) {
@@ -620,7 +621,8 @@ class ChoiceWidgetAnnotationElement extends WidgetAnnotationElement {
 
     const selectElement = document.createElement("select");
     selectElement.disabled = this.data.readOnly;
-
+    selectElement.setAttribute("name", this.data.fieldName);
+    
     if (!this.data.combo) {
       // List boxes have a size and (optionally) multiple selection.
       selectElement.size = this.data.options.length;
