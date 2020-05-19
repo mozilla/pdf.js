5a13b3b3e9dbf3bc1b99abe3bf2c3ba311797857
Jonas Jenwald <jonas.jenwald@gmail.com>
2020-05-19 15:13:43 +0200

    Convert the `src/pdf.js` and `src/pdf.worker.js` files to use standard `import`/`export` statements
    As part of reducing our reliance on SystemJS in the development viewer, this patch replaces usage of `require` statements with modern standards `import`/`export` statements instead.
    
    If we want to try and move forward with reducing usage of SystemJS, we don't have much choice but to make these kind changes (despite what prior test-results showed, however I'm no longer able to reproduce the issues locally).

--- src/pdf.worker.js
+++ src/pdf.worker.js
@@ -12,13 +12,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* eslint-disable no-unused-vars */
 
-"use strict";
+import { WorkerMessageHandler } from "./core/worker.js";
 
+/* eslint-disable-next-line no-unused-vars */
 const pdfjsVersion = PDFJSDev.eval("BUNDLE_VERSION");
+/* eslint-disable-next-line no-unused-vars */
 const pdfjsBuild = PDFJSDev.eval("BUNDLE_BUILD");
 
-const pdfjsCoreWorker = require("./core/worker.js");
-
-exports.WorkerMessageHandler = pdfjsCoreWorker.WorkerMessageHandler;
+export { WorkerMessageHandler };
