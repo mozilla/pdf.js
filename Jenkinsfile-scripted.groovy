3e08980d0e580ab808ba7ba19ac67eee273c2933
den <den.hermesss@gmail.com>
2020-07-01 15:09:52 +0300

    Jenkinsfile_groovy final

---
+++ Jenkinsfile-scripted.groovy
@@ -0,0 +1,18 @@
+node ('linux') { 
+          stage('Source') { 
+              git poll: true branch: 'denys.lapenkov_jenkins', url: 'https://github.com/Hermesss/pdf.js.git'
+          }
+          stage('Install tools') { 
+              sh '''
+               curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+                sudo apt-get install -y nodejs
+                sudo apt install -y build-essential 
+                sudo npm install -g gulp-cli 
+                sudo npm install '''
+            }
+          stage('Build') {
+                sh label: '', script: 'gulp generic'                }  
+          stage('Archive Artifacts') {
+                archiveArtifacts artifacts: 'build/generic/build/*.js*', followSymlinks: false, onlyIfSuccessful: true
+                }
+}
