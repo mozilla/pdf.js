16fa9dc4ea057fb975fa9e32e6a0773752166070
Jonas Jenwald <jonas.jenwald@gmail.com>
2020-08-06 14:36:16 +0200

    Add support for `Object.fromEntries`
    This provides a simpler way of creating an `Object` from e.g. a `Map`, without having to manually iterate over it.
    Please see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/fromEntries

--- src/shared/compatibility.js
+++ src/shared/compatibility.js
@@ -197,6 +197,15 @@ if (
     require("core-js/es/object/assign.js");
   })();
 
+  // Provides support for Object.fromEntries in legacy browsers.
+  // Support: IE, Chrome<73
+  (function checkObjectFromEntries() {
+    if (Object.fromEntries) {
+      return;
+    }
+    require("core-js/es/object/from-entries.js");
+  })();
+
   // Provides support for Math.log2 in legacy browsers.
   // Support: IE, Chrome<38
   (function checkMathLog2() {
