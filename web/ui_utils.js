34dc1aaf044eaed5df5abbe9098420328bc0ff72
Jonas Jenwald <jonas.jenwald@gmail.com>
2020-09-05 17:00:52 +0200

    Remove code-paths only relevant for IE 11/Edge (non-Chromium based) from the `web/` folder
    This patch purposely starts small, by removing IE-specific code from various JS/CSS files in the `web/` folder.
    
    There's obviously lots of potential for additional clean-up, especially the removal of no longer necessary polyfills in `src/shared/compatibility.js`, however that will require some care considering that certain polyfills may also be necessary for e.g. Node.js or the Chromium-extension as well.
    Generally speaking, once we start removing polyfills it's probably a good idea to consult the compatibility information on https://developer.mozilla.org/ and also https://caniuse.com/ first. (Deciding on the lowest supported Chromium version, for the extension, would also seem like a good idea.)

--- web/ui_utils.js
+++ web/ui_utils.js
@@ -99,7 +99,6 @@ function getOutputScale(ctx) {
   const backingStoreRatio =
     ctx.webkitBackingStorePixelRatio ||
     ctx.mozBackingStorePixelRatio ||
-    ctx.msBackingStorePixelRatio ||
     ctx.oBackingStorePixelRatio ||
     ctx.backingStorePixelRatio ||
     1;
