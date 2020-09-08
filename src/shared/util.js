147d1b27f03549b174bdda56143cdf009e0f6dfe
Brendan Dahl <brendan.dahl@gmail.com>
2020-09-08 14:24:32 -0700

    Update util.js

--- src/shared/util.js
+++ src/shared/util.js
@@ -910,6 +910,15 @@ const createObjectURL = (function createObjectURLClosure() {
   };
 })();
 
+/**
+ * AcroForm field names use an array like notation to refer to
+ * repeated XFA elements e.g. foo.bar[nnn].
+ * see: XFA Spec Chapter 3 - Repeated Elements
+ *
+ * @param {string} path - XFA path name.
+ * @returns {Array} - Array of Objects with the name and pos of
+ * each part of the path.
+ */
 function parseXFAPath(path) {
   const positionPattern = /(.+)\[([0-9]+)\]$/;
   return path.split(".").map(component => {
