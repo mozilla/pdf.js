9a51040bfb84f1bd331da82ed6685d9c4863e1b9
Xiubo Zhang <zhangxiubo@gmail.com>
2020-09-18 22:27:42 +0000

    Split only on the first `=` found in a key-value segment when parsing the parameters passed to the web viewer.

--- web/ui_utils.js
+++ web/ui_utils.js
@@ -208,7 +208,7 @@ function parseQueryString(query) {
   const parts = query.split("&");
   const params = Object.create(null);
   for (let i = 0, ii = parts.length; i < ii; ++i) {
-    const param = parts[i].split("=");
+    const param = parts[i].split(/=(.*)/);
     const key = param[0].toLowerCase();
     const value = param.length > 1 ? param[1] : null;
     params[decodeURIComponent(key)] = decodeURIComponent(value);
