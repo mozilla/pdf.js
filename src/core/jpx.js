9416b14e8b06a39a1a57f2baf22aebab2370edeb
Jonas Jenwald <jonas.jenwald@gmail.com>
2020-10-03 12:26:25 +0200

    Re-factor how the ESLint `no-var` rule is enabled in the `src/` folder
    This simplifies/consolidates the ESLint configuration slightly in the `src/` folder, and prevents the addition of any new files where `var` is being used.[1]
    Hence we no longer need to manually add `/* eslint no-var: error */` in files, which is easy to forget, and can instead disable the rule in the `src/core/` files where `var` is still in use.
    
    ---
    [1] Obviously the `no-var` rule can, in the same way as every other rule, be disabled on a case-by-case basis where actually necessary.

--- src/core/jpx.js
+++ src/core/jpx.js
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/* eslint-disable no-var */
 
 import { BaseException, info, warn } from "../shared/util.js";
 import { log2, readUint16, readUint32 } from "./core_utils.js";
