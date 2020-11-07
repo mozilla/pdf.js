457a4cd6e074a30cc6c63b37fad4d1096a568a9e
zachary <zacharykapank@gmail.com>
2020-11-07 23:33:16 +0200

    import ViewPDF component in entry App.tsx

--- examples/reactjs/src/App.tsx
+++ examples/reactjs/src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import ViewPDF from "./ViewPDF";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ViewPDF />
+    </div>
+  );
 }
 
 export default App;
