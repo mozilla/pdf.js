18b525de2e2fd7d272821a6fc4e5d99f159e7b4e
Calixte Denizet <calixte.denizet@gmail.com>
2020-11-24 18:25:26 +0100

    Parenthesis in names are not escaped when saving

--- src/core/core_utils.js
+++ src/core/core_utils.js
@@ -205,7 +205,22 @@ function escapePDFName(str) {
   let start = 0;
   for (let i = 0, ii = str.length; i < ii; i++) {
     const char = str.charCodeAt(i);
-    if (char < 0x21 || char > 0x7e || char === 0x23) {
+    // Whitespace or delimiters aren't regular chars, so escape them.
+    if (
+      char < 0x21 ||
+      char > 0x7e ||
+      char === 0x23 /* # */ ||
+      char === 0x28 /* ( */ ||
+      char === 0x29 /* ) */ ||
+      char === 0x3c /* < */ ||
+      char === 0x3e /* > */ ||
+      char === 0x5b /* [ */ ||
+      char === 0x5d /* ] */ ||
+      char === 0x7b /* { */ ||
+      char === 0x7d /* } */ ||
+      char === 0x2f /* / */ ||
+      char === 0x25 /* % */
+    ) {
       if (start < i) {
         buffer.push(str.substring(start, i));
       }
