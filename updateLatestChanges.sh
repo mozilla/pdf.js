b6497afec56127487f8c671393c99550530d40cd
stephan <webmaster1@beyondjava.de>
2020-12-30 17:41:34 +0100

    merging the latest changes of Mozilla's pdf.js

--- updateLatestChanges.sh
+++ updateLatestChanges.sh
@@ -10,7 +10,7 @@ git fetch upstream
 
 # Make sure that you're on your master branch:
 
-git checkout master
+git checkout bleeding-edge
 
 # Rewrite your master branch so that any commits of yours that
 # aren't already in upstream/master are replayed on top of that
