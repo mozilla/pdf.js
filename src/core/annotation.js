49bd1b83bff903a6694cf08f0580afb3dbdfb749
Calixte Denizet <calixte.denizet@gmail.com>
2021-01-08 16:43:16 +0100

    JS -- update radio/checkbox values even if there are no actions

--- src/core/annotation.js
+++ src/core/annotation.js
@@ -1986,7 +1986,7 @@ class ButtonWidgetAnnotation extends WidgetAnnotation {
     }
     return {
       id: this.data.id,
-      value: this.data.fieldValue || null,
+      value: this.data.fieldValue || "Off",
       defaultValue: this.data.defaultFieldValue,
       exportValues,
       editable: !this.data.readOnly,
