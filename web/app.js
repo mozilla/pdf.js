bc8ece08eb764fa445c7585f13f83475160c0f3d
Ranjan Kumar Patel <ranjanpatelbbsr@gmail.com>
2021-02-26 18:33:20 +0530

    pdfjs cors

--- web/app.js
+++ web/app.js
@@ -2346,9 +2346,9 @@ if (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) {
       // IE10 / IE11 does not include an origin in `blob:`-URLs. So don't block
       // any blob:-URL. The browser's same-origin policy will block requests to
       // blob:-URLs from other origins, so this is safe.
-      if (origin !== viewerOrigin && protocol !== "blob:") {
+      /*if (origin !== viewerOrigin && protocol !== "blob:") {
         throw new Error("file origin does not match viewer's");
-      }
+      }*/
     } catch (ex) {
       PDFViewerApplication._localizeMessage("loading_error").then(msg => {
         PDFViewerApplication._documentError(msg, { message: ex?.message });
