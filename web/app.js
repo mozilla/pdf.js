d1c7d36739e66d410a446c8b92615a9ccfecc384
Jonas Jenwald <jonas.jenwald@gmail.com>
2021-04-16 09:49:11 +0200

    Remove the special handling of "errorFontLoadNative" and "errorFontMissing" in `PDFViewerApplication.fallback` (PR 12931 follow-up, PR 12995 follow-up)
    According to a decision by UX and PM, please see https://bugzilla.mozilla.org/show_bug.cgi?id=1705060#c2 (and implemented in https://bugzilla.mozilla.org/show_bug.cgi?id=1705327), we no longer show the notification-bar in Firefox.
    Hence there's (obviously) no need to ignore certain `featureId`s in `PDFViewerApplication.fallback` anymore.

--- web/app.js
+++ web/app.js
@@ -1048,13 +1048,6 @@ const PDFViewerApplication = {
       featureId,
     });
 
-    // Don't show the fallback bar for things that are *very* unlikely to cause
-    // user-visible errors, to avoid bothering the user unnecessarily.
-    switch (featureId) {
-      case UNSUPPORTED_FEATURES.errorFontLoadNative:
-      case UNSUPPORTED_FEATURES.errorFontMissing:
-        return;
-    }
     // Only trigger the fallback once so we don't spam the user with messages
     // for one PDF.
     if (this._fellback) {
