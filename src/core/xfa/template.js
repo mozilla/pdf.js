7bb5331087248998a5821937f55f1c900d057a00
Calixte Denizet <calixte.denizet@gmail.com>
2021-07-29 14:27:17 +0200

    XFA - Avoid an error when an exdata is a string (bug 1723114)

--- src/core/xfa/template.js
+++ src/core/xfa/template.js
@@ -5905,6 +5905,9 @@ class Value extends XFAObject {
 
   [$text]() {
     if (this.exData) {
+      if (typeof this.exData[$content] === "string") {
+        return this.exData[$content].trim();
+      }
       return this.exData[$content][$text]().trim();
     }
     for (const name of Object.getOwnPropertyNames(this)) {
