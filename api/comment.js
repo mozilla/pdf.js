5fb48813d913b96700d843de2607df569ca54a9b
dialectics <897115773@qq.com>
2021-07-30 16:12:46 +0800

    test: test react bundler

---
+++ api/comment.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+module.exports = (req, res) => {
+  res.json({
+    data: [
+      {
+        content:
+          "1997 年亚洲金融危机、2003 年防治“非典”疫情、2008 年筹办北京奥运会和 2008 年爆发的国际金融危机",
+        comments:
+          "我觉得我觉得我觉得我觉得我觉得我觉得我觉得我觉得我觉得我觉得我觉得我觉得我觉得",
+      },
+      {
+        content:
+          "1955 年 1 月，北京市计划委员会正式成立，此后市政府计划主管机构几经撤并、调整， 1977 年 7 月，新组建的北京市计划委员会成立",
+        comments:
+          "我认为我认为我认为我认为我认为我认为我认为我认为我认为我认为我认为我认为我认为我认为我认为我认为",
+      },
+      {
+        content: "市发展计划委内设 14 个职能处室和机关党委",
+        comments:
+          "我想说我想说我想说我想说我想说我想说我想说我想说我想说我想说我想说我想说",
+      },
+      {
+        content: "市发展改革委直属事业单位简介如下",
+        comments:
+          "goodjobgoodjobgoodjobgoodjobgoodjobgoodjobgoodjobgoodjobgoodjobgoodjobgoodjobgoodjobgoodjobgoodjobgoodjobgoodjobgoodjobgoodjobgoodjobgoodjob",
+      },
+    ],
+  });
+};
