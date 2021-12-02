f2af3b0334178149786f77d784f8bcedc394649b
Justin Bradshaw <jjbradshaw@mastercontrol.com>
2021-12-02 09:32:12 -0700

    Small improvemente

--- web/ui_utils.js
+++ web/ui_utils.js
@@ -542,10 +542,10 @@ function isDataSchema(url) {
 
 function getParameterByName(name, url) {
   if (!url) { url = window.location.href;}
-  decodeURIComponent(url);
+  url = decodeURIComponent(url);
   name = name.replace(/[\[\]]/g, '\\$&');
-  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-    results = regex.exec(url);
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+  var results = regex.exec(url);
   if (!results) {return null;}
   if (!results[2]) {return '';}
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
