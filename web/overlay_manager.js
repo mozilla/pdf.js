c9f262c480e0d22fdcc6991700f9ce92b72812b2
Jonas Jenwald <jonas.jenwald@gmail.com>
2022-03-25 14:10:33 +0100

    Add a `<dialog>` polyfill for the `generic-legacy` build
    Please note that this patch is purposely quite basic, e.g. it doesn't add the polyfill-CSS in order to simplify the build process, and things such as `::backdrop` thus isn't working.
    However, this patch does ensure that older browsers can at least still *access* all of the previous overlays and that things like e.g. opening of password-protected documents respectively printing still works.

--- web/overlay_manager.js
+++ web/overlay_manager.js
@@ -37,6 +37,15 @@ class OverlayManager {
     }
     this.#overlays.set(dialog, { canForceClose });
 
+    if (
+      typeof PDFJSDev !== "undefined" &&
+      PDFJSDev.test("GENERIC && !SKIP_BABEL") &&
+      !dialog.showModal
+    ) {
+      const dialogPolyfill = require("dialog-polyfill/dist/dialog-polyfill.js");
+      dialogPolyfill.registerDialog(dialog);
+    }
+
     dialog.addEventListener("cancel", evt => {
       this.#active = null;
     });
