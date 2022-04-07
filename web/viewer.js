67a3f3bbd8f7eec708d8deeeebdd29f67531ae1d
Jonas Jenwald <jonas.jenwald@gmail.com>
2022-04-07 16:20:41 +0200

    Use a bit more optional chaining in the viewer

--- web/viewer.js
+++ web/viewer.js
@@ -243,9 +243,7 @@ function webViewerLoad() {
 
 // Block the "load" event until all pages are loaded, to ensure that printing
 // works in Firefox; see https://bugzilla.mozilla.org/show_bug.cgi?id=1618553
-if (document.blockUnblockOnload) {
-  document.blockUnblockOnload(true);
-}
+document.blockUnblockOnload?.(true);
 
 if (
   document.readyState === "interactive" ||
