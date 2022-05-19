cc0ffd58350faf6a12e9ab7a0629d8ea2df081a0
Nicolas Daures <daures@intuiface.com>
2022-05-19 16:44:03 +0200

    Add BrightSign support.

--- src/shared/is_node.js
+++ src/shared/is_node.js
@@ -23,6 +23,7 @@ const isNodeJS =
   typeof process === "object" &&
   process + "" === "[object process]" &&
   !process.versions.nw &&
-  !(process.versions.electron && process.type && process.type !== "browser");
+  !(process.versions.electron && process.type && process.type !== "browser") &&
+  process.argv0 !== "brightsign";
 
 export { isNodeJS };
