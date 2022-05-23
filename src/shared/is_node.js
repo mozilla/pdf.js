e051153602f939bde32e70a286eee1e22e85138b
Nicolas Daures <daures@intuiface.com>
2022-05-23 12:46:17 +0200

    Use BSDeviceInfo object to detect BrightSign OS.

--- src/shared/is_node.js
+++ src/shared/is_node.js
@@ -14,16 +14,18 @@
  */
 /* globals process */
 
-// NW.js / Electron is a browser context, but copies some Node.js objects; see
+// NW.js / Electron / BrightSign is a browser context,
+// but copies some Node.js objects; see
 // http://docs.nwjs.io/en/latest/For%20Users/Advanced/JavaScript%20Contexts%20in%20NW.js/#access-nodejs-and-nwjs-api-in-browser-context
 // https://www.electronjs.org/docs/api/process#processversionselectron-readonly
 // https://www.electronjs.org/docs/api/process#processtype-readonly
+// https://brightsign.atlassian.net/wiki/spaces/DOC/pages/370672370/BSDeviceInfo
 const isNodeJS =
   (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) &&
   typeof process === "object" &&
   process + "" === "[object process]" &&
   !process.versions.nw &&
   !(process.versions.electron && process.type && process.type !== "browser") &&
-  process.argv0 !== "brightsign";
+  !(window && window.BSDeviceInfo);
 
 export { isNodeJS };
