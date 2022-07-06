f165f9e52a48bb0597f84873c61c4e15d6293408
Jonas Jenwald <jonas.jenwald@gmail.com>
2022-07-06 23:02:53 +0200

    [editor] Use the `fit-curve` package (issue 15004)
    Rather than including all of this external code in the PDF.js repository, we should be using the npm package instead.
    Unfortunately this is slightly more complicated than you'd hope, since the `fit-curve` package (which is older) isn't directly compatible with modern JavaScript modules.
    In particular, the following cases needed to be considered:
     - For the development viewer (i.e. `gulp server`) and the unit-tests, we thus need to build a fitCurve-bundle that can be directly `import`ed.
     - For the actual PDF.js build-targets, we can slightly reduce the sizes by depending on the "raw" `fit-curve` source-code.
     - For the Node.js unit-tests, the `fit-curve` package can be used as-is.

---
+++ test/unit/editor_spec.js
@@ -0,0 +1,43 @@
+/* Copyright 2022 Mozilla Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { fitCurve } from "../../src/display/editor/ink.js";
+
+describe("editor", function () {
+  describe("fitCurve", function () {
+    it("should return a function", function () {
+      expect(typeof fitCurve).toEqual("function");
+    });
+
+    it("should compute an Array of bezier curves", function () {
+      const bezier = fitCurve(
+        [
+          [1, 2],
+          [4, 5],
+        ],
+        30,
+        null
+      );
+      expect(bezier).toEqual([
+        [
+          [1, 2],
+          [2, 3],
+          [3, 4],
+          [4, 5],
+        ],
+      ]);
+    });
+  });
+});
