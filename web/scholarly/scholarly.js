440b25b745717782b6f23a4aeaa831d618141f4b
uqxzp <uqxzp@student.kit.edu>
2022-07-12 18:28:59 +0200

    highlight implementation

--- web/scholarly/scholarly.js
+++ web/scholarly/scholarly.js
@@ -1,13 +1,11 @@
-// Stores all created highlights
+import {getColor, getMode, initUI} from "./ui.js";
+
 var highlights = [];
-// Stores all created sticky notes
-var stickyNotes = [];
-// the mouse down event for creating highlights
 let es;
 
-let tool;
-
 export function init(app) {
+  initUI();
+
   app.eventBus.on("pagerendered", function(event) {
     let canvas = event.source.canvas;
     if(event.cssTransform) {
@@ -15,39 +13,82 @@ export function init(app) {
     }
 
     drawAnnotations(event);
-
-    let highlightSelected = true;
-
-    if (highlightSelected) {
-      createHighlight(canvas, event.pageNumber);
-    }
-
-    let stickyNoteSelected = false;
-    if (stickyNoteSelected) {
-      createStickyNote(canvas);
-    }
+    createHighlight(canvas, event.pageNumber);
   });
 }
 
 function createHighlight(canvas, page) {
+  let preview;
+  let relX;
+  let relY;
+
   canvas.parentElement.parentElement.addEventListener("mousedown", (e) => {
+    if (getMode() !== "highlight") {
+      return;
+    }
+
     es = e;
     startDrag(e);
     disableSelect(e);
+
+    let bb = canvas.getBoundingClientRect();
+    relX = (e.x - bb.left) / bb.width;
+    relY = (e.y - bb.top) / bb.height;
+    preview = document.createElement("div");
+    preview.style.position = "absolute";
+    preview.style.left = (e.x - bb.left) + "px";
+    preview.style.top = (e.y - bb.top) + "px";
+    preview.style.backgroundColor = getColor();
+    preview.style.opacity = "0.2";
+    canvas.parentElement.parentElement.querySelector(".annotationEditorLayer").appendChild(preview);
+  })
+
+  canvas.parentElement.parentElement.addEventListener("mousemove", (e) => {
+    if (getMode() !== "highlight" || preview == null) {
+      return;
+    }
+
+    let bb = canvas.getBoundingClientRect();
+    let endRelX = (e.x - bb.left) / bb.width;
+    let endRelY = (e.y - bb.top) / bb.height;
+    let relWidth = endRelX - relX;
+    let relHeight = endRelY - relY;
+    let absWidth = relWidth * bb.width;
+    let absHeight = relHeight * bb.height;
+    preview.style.width = Math.abs(absWidth) + "px";
+    preview.style.height = Math.abs(absHeight) + "px";
+
+    if (absWidth < 0) {
+      preview.style.left = (e.x - bb.left) + "px";
+    } else {
+      preview.style.left = (relX * bb.width) + "px";
+    }
+
+    if (absHeight < 0) {
+      preview.style.top = (e.y - bb.top) + "px";
+    } else {
+      preview.style.top = (relY * bb.height) + "px";
+    }
+
   })
 
   canvas.parentElement.parentElement.addEventListener("mouseup", (ee) => {
-    onDragEnd();
+    if (getMode() !== "highlight") {
+      return;
+    }
 
+    onDragEnd();
     let bb = canvas.getBoundingClientRect();
     let relX = (es.x - bb.left) / bb.width;
     let relY = (es.y - bb.top) / bb.height;
     let relW = (ee.x - es.x) / bb.width;
     let relH = (ee.y - es.y) / bb.height;
-    let color = { r: 0, g: 255, b: 90 };
+    let color = getColor();
     renderRect(canvas, relX, relY, relW, relH, color);
-
     highlights.push( { page, relPos: { x: relX, y: relY }, relSize: { width: relW, height: relH }, color } );
+
+    preview = null;
+    canvas.parentElement.parentElement.querySelector(".annotationEditorLayer").innerHTML = "";
   })
 }
 
@@ -66,12 +107,6 @@ function drawAnnotations(event) {
   highlights.forEach(element => {
     if (element.page === event.pageNumber) {
       renderHighlight(event.source.canvas, element.relPos, element.relSize, element.color, "Fritz", "abc");
-    }
-  });
-
-  stickyNotes.forEach(element => {
-    if (element.page === event.page) {
-
     }
   });
 }
@@ -108,11 +143,9 @@ function renderRect(canvas, relX, relY, relW, relH, color) {
   let absY = canvas.height * relY;
   let absW = canvas.width * relW;
   let absH = canvas.height * relH;
-  let r = color.r;
-  let g = color.g;
-  let b = color.b;
 
-  ctx.fillStyle = `rgba(${r}, ${g}, ${b}, 0.2)`;
+  ctx.fillStyle = color;
+  ctx.globalAlpha = 0.2;
   ctx.fillRect(absX, absY, absW, absH);
 }
 
