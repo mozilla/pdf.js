af41a5cb4933f7a3024df5e98806291bf159963f
Calixte Denizet <calixte.denizet@gmail.com>
2022-07-21 18:31:09 +0200

    [Editor] Simplify the command manager
    The previous version was maybe functional but definitely painful to maintain
    (maybe more efficient... I don't know) so this patch aims to simplify it and
    it adds some basic unit tests.

--- test/unit/editor_spec.js
+++ test/unit/editor_spec.js
@@ -13,9 +13,85 @@
  * limitations under the License.
  */
 
+import { CommandManager } from "../../src/display/editor/tools.js";
 import { fitCurve } from "../../src/display/editor/ink.js";
 
 describe("editor", function () {
+  describe("Command Manager", function () {
+    it("should check undo/redo", function () {
+      const manager = new CommandManager(4);
+      let x = 0;
+      const makeDoUndo = n => ({ cmd: () => (x += n), undo: () => (x -= n) });
+
+      manager.add({ ...makeDoUndo(1), mustExec: true });
+      expect(x).toEqual(1);
+
+      manager.add({ ...makeDoUndo(2), mustExec: true });
+      expect(x).toEqual(3);
+
+      manager.add({ ...makeDoUndo(3), mustExec: true });
+      expect(x).toEqual(6);
+
+      manager.undo();
+      expect(x).toEqual(3);
+
+      manager.undo();
+      expect(x).toEqual(1);
+
+      manager.undo();
+      expect(x).toEqual(0);
+
+      manager.undo();
+      expect(x).toEqual(0);
+
+      manager.redo();
+      expect(x).toEqual(1);
+
+      manager.redo();
+      expect(x).toEqual(3);
+
+      manager.redo();
+      expect(x).toEqual(6);
+
+      manager.redo();
+      expect(x).toEqual(6);
+
+      manager.undo();
+      expect(x).toEqual(3);
+
+      manager.redo();
+      expect(x).toEqual(6);
+    });
+  });
+
+  it("should hit the limit of the manager", function () {
+    const manager = new CommandManager(3);
+    let x = 0;
+    const makeDoUndo = n => ({ cmd: () => (x += n), undo: () => (x -= n) });
+
+    manager.add({ ...makeDoUndo(1), mustExec: true }); // 1
+    manager.add({ ...makeDoUndo(2), mustExec: true }); // 3
+    manager.add({ ...makeDoUndo(3), mustExec: true }); // 6
+    manager.add({ ...makeDoUndo(4), mustExec: true }); // 10
+    expect(x).toEqual(10);
+
+    manager.undo();
+    manager.undo();
+    expect(x).toEqual(3);
+
+    manager.undo();
+    expect(x).toEqual(1);
+
+    manager.undo();
+    expect(x).toEqual(1);
+
+    manager.redo();
+    manager.redo();
+    expect(x).toEqual(6);
+    manager.add({ ...makeDoUndo(5), mustExec: true });
+    expect(x).toEqual(11);
+  });
+
   describe("fitCurve", function () {
     it("should return a function", function () {
       expect(typeof fitCurve).toEqual("function");
