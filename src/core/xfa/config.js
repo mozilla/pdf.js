4de07465196b7026f3eba1e952647c582b710598
Jonas Jenwald <jonas.jenwald@gmail.com>
2022-09-19 16:29:34 +0200

    Enable the `unicorn/prefer-regexp-test` ESLint plugin rule
    Please see https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/prefer-regexp-test.md

--- src/core/xfa/config.js
+++ src/core/xfa/config.js
@@ -1005,7 +1005,7 @@ class Rename extends ContentObject {
     // is no colon.
     if (
       this[$content].toLowerCase().startsWith("xml") ||
-      this[$content].match(new RegExp("[\\p{L}_][\\p{L}\\d._\\p{M}-]*", "u"))
+      new RegExp("[\\p{L}_][\\p{L}\\d._\\p{M}-]*", "u").test(this[$content])
     ) {
       warn("XFA - Rename: invalid XFA name");
     }
