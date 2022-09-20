f5b835157b4493db9955418596c7934eef22f6c6
Calixte Denizet <calixte.denizet@gmail.com>
2022-09-20 13:53:50 +0200

    [XFA] Fix an hidden issue in the FormCalc lexer
    Since there are no script engine with XFA, the FormCalc parser is not used irl.
    The bug @nmtigor noticed was hidden by another one (the wrong check on `match`).

--- src/core/xfa/formcalc_lexer.js
+++ src/core/xfa/formcalc_lexer.js
@@ -239,8 +239,8 @@ class Lexer {
 
   getNumber(first) {
     const match = this.data.substring(this.pos).match(numberPattern);
-    if (!match) {
-      return first - 0x30 /* = 0 */;
+    if (!match[0]) {
+      return new Token(TOKEN.number, first - 0x30 /* = 0 */);
     }
     const number = parseFloat(
       this.data.substring(this.pos - 1, this.pos + match[0].length)
