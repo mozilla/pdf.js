b659bacc43726de233fe1c28fd17d57bd726d788
Jonas Jenwald <jonas.jenwald@gmail.com>
2022-12-03 11:54:56 +0100

    Tighten the `vars`-argument for the ESLint `no-unused-vars` rule
    Please see https://eslint.org/docs/latest/rules/no-unused-vars#vars

--- extensions/chromium/preserve-referer.js
+++ extensions/chromium/preserve-referer.js
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 /* import-globals-from pdfHandler.js */
+/* exported saveReferer */
 
 "use strict";
 /**
