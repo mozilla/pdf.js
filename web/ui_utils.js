9d5085347af995d2bb5a690e8b8362302983f0ff
Jonas Jenwald <jonas.jenwald@gmail.com>
2023-01-25 11:09:28 +0100

    Move `ProgressBar`-related CSS variables into the `loadingBar` DOM-element (issue 15958)
    This way we avoid reflowing the entire viewer when e.g. updating the loading progress.

--- web/ui_utils.js
+++ web/ui_utils.js
@@ -697,10 +697,13 @@ class ProgressBar {
 
   #percent = 0;
 
+  #style = null;
+
   #visible = true;
 
   constructor(bar) {
     this.#classList = bar.classList;
+    this.#style = bar.style;
   }
 
   get percent() {
@@ -716,7 +719,7 @@ class ProgressBar {
     }
     this.#classList.remove("indeterminate");
 
-    docStyle.setProperty("--progressBar-percent", `${this.#percent}%`);
+    this.#style.setProperty("--progressBar-percent", `${this.#percent}%`);
   }
 
   setWidth(viewer) {
@@ -726,7 +729,10 @@ class ProgressBar {
     const container = viewer.parentNode;
     const scrollbarWidth = container.offsetWidth - viewer.offsetWidth;
     if (scrollbarWidth > 0) {
-      docStyle.setProperty("--progressBar-end-offset", `${scrollbarWidth}px`);
+      this.#style.setProperty(
+        "--progressBar-end-offset",
+        `${scrollbarWidth}px`
+      );
     }
   }
 
