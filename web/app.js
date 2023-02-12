cef417e44a328a27a28519c32b4abc547931b47e
Jonas Jenwald <jonas.jenwald@gmail.com>
2023-02-12 15:48:05 +0100

    Remove the unused `eventBus` option in `PDFThumbnailViewer` (PR 15215 follow-up)
    This became unused with PR 15215, but I unfortunately forgot to remove it.

--- web/app.js
+++ web/app.js
@@ -532,7 +532,6 @@ const PDFViewerApplication = {
     if (appConfig.sidebar?.thumbnailView) {
       this.pdfThumbnailViewer = new PDFThumbnailViewer({
         container: appConfig.sidebar.thumbnailView,
-        eventBus,
         renderingQueue: pdfRenderingQueue,
         linkService: pdfLinkService,
         l10n: this.l10n,
