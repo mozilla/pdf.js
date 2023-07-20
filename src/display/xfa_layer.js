3f4447c482e34b3c1de5cb49f8d5ec28267f1578
TaTo30 <aldo.hernandez3012@gmail.com>
2023-07-19 19:18:40 -0600

    [XFA] Update select and option element attributes with the stored data and removes the 'selected' attribute from option element if it's not actually selected.

--- src/display/xfa_layer.js
+++ src/display/xfa_layer.js
@@ -79,9 +79,12 @@ class XfaLayer {
         break;
       case "select":
         if (storedData.value !== null) {
+          html.setAttribute("value", storedData.value);
           for (const option of element.children) {
             if (option.attributes.value === storedData.value) {
               option.attributes.selected = true;
+            } else if (option.attributes.hasOwnProperty("selected")) {
+              delete option.attributes.selected;
             }
           }
         }
