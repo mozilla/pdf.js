a0cbb7ef5436f47f3f70f1d9858e04e20ba44182
Calixte Denizet <calixte.denizet@gmail.com>
2023-07-30 15:52:27 +0200

    Don't print hidden annotatons (bug 1815196)
    and handle correctly the NoView and NoPrint flags when they're changed
    from JS.

--- src/core/document.js
+++ src/core/document.js
@@ -556,7 +556,8 @@ class Page {
       for (const annotation of annotations) {
         if (
           intentAny ||
-          (intentDisplay && annotation.mustBeViewed(annotationStorage)) ||
+          (intentDisplay &&
+            annotation.mustBeViewed(annotationStorage, renderForms)) ||
           (intentPrint && annotation.mustBePrinted(annotationStorage))
         ) {
           opListPromises.push(
