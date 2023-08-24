55744027784747925c72c19b5a09ec26f6e3db88
prefixoid <prefixoid@gmail.com>
2023-08-24 12:23:05 +0200

    update current page added

--- web/toolbar.js
+++ web/toolbar.js
@@ -127,6 +127,19 @@ class Toolbar {
     this.pageNumber = pageNumber;
     this.pageLabel = pageLabel;
     this.#updateUIState(false);
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const bookPath = urlParams.get('file');
+    var bookName = bookPath.substring(
+      bookPath.lastIndexOf("/") + 1, 
+      bookPath.lastIndexOf(".")
+    );
+    const updateCurrentPageUrl = "https://bulba.site/lib2/engine/back/update-current-page.php?book=" + 
+      bookName + "&current-page=" + pageNumber;
+    fetch(updateCurrentPageUrl, {
+      method: "GET"
+    });
   }
 
   setPagesCount(pagesCount, hasPageLabels) {
