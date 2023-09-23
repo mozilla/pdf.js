b8540248204dc318627226a301ca5cad10b02c73
maxidrom <maskimka@inbox.ru>
2023-09-23 14:26:39 +0200

    Add files via upload (#8)

--- back/book-shelf.php
+++ back/book-shelf.php
@@ -1,6 +1,42 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Book List</title>
+<style>
+    ul {
+        list-style: none;
+        padding: 0;
+    }
+    li {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 10px;
+        border-bottom: 1px solid #ccc;
+    }
+    button {
+        padding: 5px 10px;
+        cursor: pointer;
+    }
+    
+    /*make links as regular text*/
+    /* Remove default link styling */
+    a {
+        text-decoration: none;
+        color: inherit; /* Use the parent element's text color */
+        cursor: pointer; /* Change cursor to indicate interactivity */
+    }
+    
+    /* Define a hover effect */
+    a:hover {
+        text-decoration: none; /* Add underline on hover */
+    }
+</style>
+</head>
 <body>
+<h1>Book List</h1>
 <?php      
     //read email
     session_start();
@@ -16,24 +52,40 @@
         mkdir($dir_path, 0777, true);
     }
     $files = array_diff(scandir($dir_path), array('..', '.'));
-    echo "<ul>";
+    echo '<ul id="bookList">';
     foreach ($files as $file_name) {
         if(is_file($dir_path . "/" . $file_name)) {
             $book_name = substr($file_name, 0, strpos($file_name, ".pdf"));
-            $str = '<li><a href="https://bulba.site/lib2/engine/back/open-book.php?book=' . $book_name . '">' . $book_name . '</a>    
-                <a href="https://bulba.site/lib2/engine/back/delete.php?book=' . $book_name . '">del</a></li>';
+            $str = '<li><span><a href="https://bulba.site/lib2/engine/back/open-book.php?book=' . $book_name . '">' . $book_name . '</a></span>   
+            <button onclick="location.href=\'https://bulba.site/lib2/engine/back/delete.php?book=' . $book_name . '\'" type="button">Delete</button></li>';
             print($str);
         }
     }
     echo "</ul>";
     
     
-print('<br><form action="upload.php?email='. $email .'" method="post" enctype="multipart/form-data">');
+print("<br>Upload book");
+print('<form action="upload.php?email='. $email .'" method="post" enctype="multipart/form-data">');
 ?>
-  Select pdf file to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload" name="submit">
+<!--style="width: 85px;"-->
+<input type="file"    name="fileToUpload" id="fileToUpload" onchange="this.form.submit()">
 </form>
+<!--<button id="uploadButton">Upload Book</button>
+<script>
+    // Event listener for upload button
+    uploadButton.addEventListener('click', () => {
+      fileInput.click();
+    });
+    
+    // Event listener for file input change
+    fileInput.addEventListener('change', event => {
+        const selectedFile = event.target.files[0];
+        if (selectedFile) {
+                window.open(url="upload.php?email=<? echo $email ?>&file=" + selectedFile.name);
+        }
+    });
+</script>
+</form>-->
 
 </body>
 </html>
--- back/open-book.php
+++ back/open-book.php
@@ -12,16 +12,18 @@
         echo "Variable not set.";
         exit('email not set');
     }
-    
+        
     $shelf_path = "../../shelf/private/" . $email ."/";
     $book_current_page_file_path = $shelf_path . "curpages/" . $book;
     if (file_exists($book_current_page_file_path)) {
-        $myfile = fopen($book_current_page_file_path, r) or die("Unable to open file:" . $book_current_page_file_path);
-        $current_page = fgets($myfile);
+        $myfile = fopen($book_current_page_file_path, "r") or die("Unable to open file:" . $book_current_page_file_path);
+        $current_page = trim(fgets($myfile));
+        $top_offset = trim(fgets($myfile));
     } else {
         $current_page = 1;
+        $top_offset = 792; //page start
     }
-    
-    $location = 'https://bulba.site/lib2/engine/front/web/viewer.html?file=../' . $shelf_path . $book . '.pdf#page=' . $current_page;
+
+    $location = "../front/web/viewer.html?file=../" . $shelf_path . $book . '.pdf#page=' . $current_page . '&zoom=100,0,' . $top_offset; //TODO: save zoom as well
     header('Location: ' . $location);
 ?>
