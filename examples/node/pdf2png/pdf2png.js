67324899c587538ffe55b3bc252f72835f77b9a6
Jonas Jenwald <jonas.jenwald@gmail.com>
2023-10-14 12:38:17 +0200

    Enable the `import/no-commonjs` ESLint plugin rule
    Given the amount of work put into removing `require`-calls from the code-base, let's ensure that new ones aren't accidentally added in the future.
    
    Note that we still have a couple of files where `require` is being used, in particular:
     - The Node.js examples, however those will be updated to use `import` in PR 17081.
     - The Webpack examples, and related support files, however I unfortunately don't know enough about Webpack to be able to update those. (Hopefully users of that code will help out here, once version `4` is released.)
     - The `statcmp`-tool, since *some* of those `require`-calls cannot be converted to `import` without other code changes (and that file is only used during benchmarking).
    
    Please find additional details at https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-commonjs.md

--- examples/node/pdf2png/pdf2png.js
+++ examples/node/pdf2png/pdf2png.js
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/* eslint-disable import/no-commonjs */
 
 const Canvas = require("canvas");
 const assert = require("assert").strict;
