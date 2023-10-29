88bad0ff7e6cbabcab96e85a36cfaf1fbc90a835
Alexander Leon <alexanderleon@Alexanders-MacBook-Pro.local>
2023-10-29 15:51:54 -0400

    w

--- src/display/editor/annotation_editor_layer.js
+++ src/display/editor/annotation_editor_layer.js
@@ -734,11 +734,9 @@ class AnnotationEditorLayer {
     }
     this.updateMode();
     if (this.#initialAnnotationsAdded) {
-      return;
+      // return;
     }
-    console.log(annotations, 'annotations3')
     if (!!annotations?.length) {
-      console.log(annotations, 'drawing ann4')
       for (const annotation of annotations) {
         const editor = new FreeTextEditor({
           parent: this,
