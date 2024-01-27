8d3ee1b6c240e455567b4e7462b51da9f2cd1d14
Jonas Jenwald <jonas.jenwald@gmail.com>
2024-01-27 13:12:54 +0100

    Stub out the `getNimbusExperimentData` method in "regular" MOZCENTRAL builds

--- web/firefoxcom.js
+++ web/firefoxcom.js
@@ -400,6 +400,9 @@ class ExternalServices extends BaseExternalServices {
   }
 
   async getNimbusExperimentData() {
+    if (typeof PDFJSDev === "undefined" || !PDFJSDev.test("GECKOVIEW")) {
+      return null;
+    }
     const nimbusData = await FirefoxCom.requestAsync(
       "getNimbusExperimentData",
       null
