4541fea1624e42765529ee03a30b6d39ded6f23b
Jonas Jenwald <jonas.jenwald@gmail.com>
2024-02-02 12:00:06 +0100

    [api-minor] Remove the, now unused, `pdfjsLibPromise` global
    This global was only introduced to work-around problems caused by the GENERIC PDF.js build using top level await. Since that was removed in the previous commit, this global is now dead code.

--- web/pdfjs.js
+++ web/pdfjs.js
@@ -13,15 +13,6 @@
  * limitations under the License.
  */
 
-// Ensure that the viewer waits for the library to complete loading,
-// to avoid breaking e.g. the standalone viewer components (see issue 17228).
-if (
-  (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) &&
-  !globalThis.pdfjsLib
-) {
-  await globalThis.pdfjsLibPromise;
-}
-
 const {
   AbortException,
   AnnotationEditorLayer,
