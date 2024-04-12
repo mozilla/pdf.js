60832fee27acca08fce006a0a01dad4e8b55931b
Jonas Jenwald <jonas.jenwald@gmail.com>
2024-04-12 13:16:44 +0200

    Fix resetting of cursor-tools when closing the document (PR 17464 follow-up)
    With the removal of the "secondarytoolbarreset" event in PR 17464 resetting of cursor-tools state accidentally broke.

--- web/secondary_toolbar.js
+++ web/secondary_toolbar.js
@@ -182,6 +182,7 @@ class SecondaryToolbar {
     this.#updateUIState();
 
     // Reset the Scroll/Spread buttons too, since they're document specific.
+    this.eventBus.dispatch("switchcursortool", { source: this, reset: true });
     this.#scrollModeChanged({ mode: ScrollMode.VERTICAL });
     this.#spreadModeChanged({ mode: SpreadMode.NONE });
   }
