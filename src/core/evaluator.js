ac182e8e32eeecfff8ce6b24380b809e3ef99441
Calixte Denizet <calixte.denizet@gmail.com>
2024-04-27 17:13:34 +0200

    Simplify the way to pass the glyph drawing instructions from the worker to the main thread
    and remove the use of eval in the font loader.

--- src/core/evaluator.js
+++ src/core/evaluator.js
@@ -4391,6 +4391,15 @@ class PartialEvaluator {
       }
     }
 
+    let fontMatrix = dict.getArray("FontMatrix");
+    if (
+      !Array.isArray(fontMatrix) ||
+      fontMatrix.length !== 6 ||
+      fontMatrix.some(x => typeof x !== "number")
+    ) {
+      fontMatrix = FONT_IDENTITY_MATRIX;
+    }
+
     const properties = {
       type,
       name: fontName.name,
@@ -4403,7 +4412,7 @@ class PartialEvaluator {
       loadedName: baseDict.loadedName,
       composite,
       fixedPitch: false,
-      fontMatrix: dict.getArray("FontMatrix") || FONT_IDENTITY_MATRIX,
+      fontMatrix,
       firstChar,
       lastChar,
       toUnicode,
