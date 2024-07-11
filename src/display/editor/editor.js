7f4a2e43b3fa1e6f57cd416f856452ee9e5c7007
Calixte Denizet <calixte.denizet@gmail.com>
2024-07-11 22:31:55 +0200

    [Editor] Make highlight annotations editable (bug 1883884)
    The goal of this patch is to be able to edit existing highlight annotations.

--- src/display/editor/editor.js
+++ src/display/editor/editor.js
@@ -1367,6 +1367,7 @@ class AnnotationEditor {
       data.rect,
       pageHeight
     );
+
     editor.x = x / pageWidth;
     editor.y = y / pageHeight;
     editor.width = width / pageWidth;
@@ -1765,7 +1766,7 @@ class AnnotationEditor {
   /**
    * Render an annotation in the annotation layer.
    * @param {Object} annotation
-   * @returns {HTMLElement}
+   * @returns {HTMLElement|null}
    */
   renderAnnotationElement(annotation) {
     let content = annotation.container.querySelector(".annotationContent");
