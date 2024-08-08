be93d530deab4a1d9a4ad795ee0c80b7fa9ae119
Tim van der Meij <timvandermeij@gmail.com>
2024-08-08 19:51:20 +0200

    Remove the `eslint-plugin-mozilla` dependency
    We only used two rules from the plugin which can both easily be replaced
    nowadays:
    
    - `mozilla/avoid-removeChild` is equal to the already active
      `unicorn/prefer-dom-node-remove` rule; please see [1] and [2].
    - `mozilla/import-globals` is mostly obsolete nowadays ever since we
      removed the Firefox extension code from this repository and we
      eliminated most globals usage. The three remaining occurrences can be
      replaced with explicit `/* globals */` comments that we already use
      elsewhere, which overall is also more consistent.
    
    [1] https://firefox-source-docs.mozilla.org/code-quality/lint/linters/eslint-plugin-mozilla/rules/avoid-removeChild.html
    [2] https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/prefer-dom-node-remove.md

--- extensions/chromium/pdfHandler-vcros.js
+++ extensions/chromium/pdfHandler-vcros.js
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 /* eslint strict: ["error", "function"] */
-/* import-globals-from pdfHandler.js */
+/* globals getViewerURL */
 
 (function () {
   "use strict";
