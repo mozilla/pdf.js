796c037197e46381f58ac16d8c1ae7952dd1312d
Calixte Denizet <calixte.denizet@gmail.com>
2024-08-30 19:18:36 +0200

    Update svglint to 3.1.0
    The new release contains:
     - https://github.com/simple-icons/svglint/pull/110
     - https://github.com/simple-icons/svglint/pull/111
    
    which allow us to slightly simplify our code.

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1983,8 +1983,9 @@ gulp.task("lint", function (done) {
 
   const svgLintOptions = [
     "node_modules/svglint/bin/cli.js",
-    "web/**/*.svg",
+    "**/*.svg",
     "--ci",
+    "--no-summary",
   ];
 
   const esLintProcess = startNode(esLintOptions, { stdio: "inherit" });
@@ -2009,12 +2010,7 @@ gulp.task("lint", function (done) {
         }
 
         const svgLintProcess = startNode(svgLintOptions, {
-          stdio: "pipe",
-        });
-        svgLintProcess.stdout.setEncoding("utf8");
-        svgLintProcess.stdout.on("data", m => {
-          m = m.toString().replace(/-+ Summary -+.*/ms, "");
-          console.log(m);
+          stdio: "inherit",
         });
         svgLintProcess.on("close", function (svgLintCode) {
           if (svgLintCode !== 0) {
