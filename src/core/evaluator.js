471284f51b3e89eac74af038332052b99a3e2298
Jonas Jenwald <jonas.jenwald@gmail.com>
2024-11-14 11:47:49 +0100

    [api-minor] Disable `ImageDecoder` usage by default in Chromium browsers
    Given that there are multiple issues with `ImageDecoder` in Chromium browsers, affecting both BMP and JPEG images, for now we (by default) disable that functionality there to avoid problems.
    
    This also means that we can remove the previously added, and separate, `isChrome` API-option.

--- src/core/evaluator.js
+++ src/core/evaluator.js
@@ -85,7 +85,6 @@ const DefaultPartialEvaluatorOptions = Object.freeze({
   isEvalSupported: true,
   isOffscreenCanvasSupported: false,
   isImageDecoderSupported: false,
-  isChrome: false,
   canvasMaxAreaInBytes: -1,
   fontExtraProperties: false,
   useSystemFonts: true,
