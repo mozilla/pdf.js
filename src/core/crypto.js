6a864e47fe17899915be9a970b389ecb4f8e2173
Jonas Jenwald <jonas.jenwald@gmail.com>
2025-02-09 11:42:17 +0100

    Use `crypto.getRandomValues` unconditionally in the `src/core/crypto.js` file
    This functionality is now available in all browsers/environments that we support, please see https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues#browser_compatibility

--- src/core/crypto.js
+++ src/core/crypto.js
@@ -1397,13 +1397,7 @@ class CipherTransform {
 
       // Generate an initialization vector
       const iv = new Uint8Array(16);
-      if (typeof crypto !== "undefined") {
-        crypto.getRandomValues(iv);
-      } else {
-        for (let i = 0; i < 16; i++) {
-          iv[i] = Math.floor(256 * Math.random());
-        }
-      }
+      crypto.getRandomValues(iv);
 
       let data = stringToBytes(s);
       data = cipher.encrypt(data, iv);
