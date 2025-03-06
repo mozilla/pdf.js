04c719015e8c0769c7f88b902215dddac195fa8f
Ritchie Zhu <laozhu.me@gmail.com>
2025-03-06 16:01:42 +0800

    add default pdf file

--- web/app_options.js
+++ web/app_options.js
@@ -488,7 +488,7 @@ if (typeof PDFJSDev === "undefined" || !PDFJSDev.test("MOZCENTRAL")) {
     value:
       typeof PDFJSDev !== "undefined" && PDFJSDev.test("CHROME")
         ? ""
-        : "compressed.tracemonkey-pldi-09.pdf",
+        : "contract-of-sale-2-4-jansson-road-rhyll-vic-3923-v2.pdf",
     kind: OptionKind.VIEWER,
   };
   defaultOptions.sandboxBundleSrc = {
