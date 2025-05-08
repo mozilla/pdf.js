febc6b76f1d63622f32ae7424e47b10dd9047fae
Thomas Hackl <hackl@data-quest.de>
2025-05-08 11:51:45 +0200

    Changes for Stud.IP integration

--- web/app.js
+++ web/app.js
@@ -71,6 +71,7 @@ import { LinkTarget, PDFLinkService } from "./pdf_link_service.js";
 import { AltTextManager } from "web-alt_text_manager";
 import { AnnotationEditorParams } from "web-annotation_editor_params";
 import { CaretBrowsingMode } from "./caret_browsing.js";
+import { CommentTextManager } from "./comment_text_manager.js";
 import { DownloadManager } from "web-download_manager";
 import { EditorUndoBar } from "./editor_undo_bar.js";
 import { OverlayManager } from "./overlay_manager.js";
@@ -485,6 +486,15 @@ const PDFViewerApplication = {
           )
         : null;
 
+    const commentTextManager = appConfig.commentTextDialog
+      ? new CommentTextManager(
+        appConfig.commentTextDialog,
+        container,
+        this.overlayManager,
+        eventBus
+      )
+      : null;
+
     const enableHWA = AppOptions.get("enableHWA"),
       maxCanvasPixels = AppOptions.get("maxCanvasPixels"),
       maxCanvasDim = AppOptions.get("maxCanvasDim"),
@@ -499,6 +509,7 @@ const PDFViewerApplication = {
       altTextManager,
       signatureManager,
       editorUndoBar: this.editorUndoBar,
+      commentTextManager,
       findController,
       scriptingManager:
         AppOptions.get("enableScripting") && pdfScriptingManager,
