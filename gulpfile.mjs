a730cba414664032b0ccc77d33e3456922740fd8
Tim van der Meij <timvandermeij@gmail.com>
2025-09-14 18:44:31 +0200

    Update dependencies to the most recent versions

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2340,7 +2340,7 @@ function packageJson() {
     bugs: DIST_BUGS_URL,
     license: DIST_LICENSE,
     optionalDependencies: {
-      "@napi-rs/canvas": "^0.1.78",
+      "@napi-rs/canvas": "^0.1.80",
     },
     browser: {
       canvas: false,
