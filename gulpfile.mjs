6bd5c7cdb3dbca50fd1106c39a596609cea7a043
Tim van der Meij <timvandermeij@gmail.com>
2025-10-12 20:30:44 +0200

    Update dependencies to the most recent versions

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -96,7 +96,7 @@ const AUTOPREFIXER_CONFIG = {
 const BABEL_TARGETS = ENV_TARGETS.join(", ");
 
 const BABEL_PRESET_ENV_OPTS = Object.freeze({
-  corejs: "3.45.1",
+  corejs: "3.46.0",
   exclude: ["web.structured-clone"],
   shippedProposals: true,
   useBuiltIns: "usage",
