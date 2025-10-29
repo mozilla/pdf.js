aeceee1df3362de4c22e834da230ba408baaae5c
calixteman <cdenizet@mozilla.com>
2025-10-29 15:41:34 +0100

    Revert "Add some telemetry in order to know what are the certificates used in pdfs (bug 1973573)"

--- web/app.js
+++ web/app.js
@@ -1718,13 +1718,6 @@ const PDFViewerApplication = {
     if (pdfDocument !== this.pdfDocument) {
       return; // The document was closed while the metadata resolved.
     }
-    if (info.collectedSignatureCertificates) {
-      this.externalServices.reportTelemetry({
-        type: "signatureCertificates",
-        data: info.collectedSignatureCertificates,
-      });
-    }
-
     this.documentInfo = info;
     this.metadata = metadata;
     this._contentDispositionFilename ??= contentDispositionFilename;
