2f2d5c9e278290ed8701f320cfd56d73437169ff
Nicolò Ribaudo <nribaudo@igalia.com>
2026-03-03 11:21:29 +0100

    Add script to check license headers

--- test/font/fontutils.js
+++ test/font/fontutils.js
@@ -1,5 +1,4 @@
-/*
- * Copyright 2013 Mozilla Foundation
+/* Copyright 2013 Mozilla Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
