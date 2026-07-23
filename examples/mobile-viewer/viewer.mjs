674944d5e10462db0aede791e2a5b6f3a8420920
ChaoticNeutralCzech <92913243+ChaoticNeutralCzech@users.noreply.github.com>
2026-07-23 11:55:42 +0200

    Undo viewer.mjs TODO mark
    Line 80 in this file not correct place to do this

--- examples/mobile-viewer/viewer.mjs
+++ examples/mobile-viewer/viewer.mjs
@@ -77,7 +77,7 @@ const PDFViewerApplication = {
         });
 
         this.loadingBar.hide();
-        this.setTitleUsingMetadata(pdfDocument); // TODO: check for option to disable this. I'd add if (!AppOptions.get("ignorePdfTitle")) but IDK if it's available in this context 
+        this.setTitleUsingMetadata(pdfDocument);
       },
       reason => {
         let key = "pdfjs-loading-error";
