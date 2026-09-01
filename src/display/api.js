a4086fe953f9f04e13212239b9ecb400c04ecbd2
Aditi <aditisingh1400@gmail.com>
2026-09-01 07:17:13 +0530

    Enable WebGPU in the renderer worker
    When the enableWebGPU option is set, the renderer worker initialises
    its own GPU device before creating its rendering surfaces, in the same
    way that getDocument starts the GPU initialisation on the main thread.
    
    This commit is a part of the renderer-worker series.

--- src/display/api.js
+++ src/display/api.js
@@ -408,6 +408,7 @@ function getDocument(src = {}) {
     pdfBug,
     styleElement,
     enableHWA,
+    enableWebGPU,
     loadingParams: {
       disableAutoFetch,
       enableXfa,
@@ -1677,6 +1678,7 @@ class PDFPageProxy {
       pdfBug: this._pdfBug,
       pageColors,
       enableHWA: this._transport.enableHWA,
+      enableWebGPU: this._transport.enableWebGPU,
       operationsFilter,
       rendererWorker: this._transport.rendererWorker,
     });
@@ -2653,6 +2655,7 @@ class WorkerTransport {
       styleElement: params.styleElement,
     });
     this.enableHWA = params.enableHWA;
+    this.enableWebGPU = params.enableWebGPU === true;
     this.rendererWorker = params.rendererWorker || null;
     this.loadingParams = params.loadingParams;
     this._params = params;
@@ -3587,6 +3590,7 @@ class InternalRenderTask {
     pdfBug = false,
     pageColors = null,
     enableHWA = false,
+    enableWebGPU = false,
     operationsFilter = null,
     rendererWorker = null,
   }) {
@@ -3620,6 +3624,7 @@ class InternalRenderTask {
     this._canvas = params.canvas;
     this._canvasContext = params.canvas ? null : params.canvasContext;
     this._enableHWA = enableHWA;
+    this._enableWebGPU = enableWebGPU;
     this._recordOperations = !!params.recordOperations;
     this._recordImages = !!params.recordImages;
     this._recordForDebugger = !!params.recordForDebugger;
@@ -3747,6 +3752,7 @@ class InternalRenderTask {
           pageId: this._pageId,
           renderTaskId: this._renderTaskId,
           enableHWA: this._enableHWA,
+          enableWebGPU: this._enableWebGPU,
           hasAnnotationCanvasMap: !!this.annotationCanvasMap,
           recordOperations: this._recordOperations,
           recordImages: this._recordImages,
