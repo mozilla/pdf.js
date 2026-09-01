77fae9b0a965731216d91fca5cc89efe4af5b729
Aditi <aditisingh1400@gmail.com>
2026-09-01 07:17:13 +0530

    Enable WebGPU in the renderer worker
    When the enableWebGPU option is set, the renderer worker initialises
    its own GPU device before creating its rendering surfaces, in the same
    way that getDocument starts the GPU initialisation on the main thread.
    
    This commit is a part of the renderer-worker series.

--- src/display/api.js
+++ src/display/api.js
@@ -410,6 +410,7 @@ function getDocument(src = {}) {
     pdfBug,
     styleElement,
     enableHWA,
+    enableWebGPU,
     loadingParams: {
       disableAutoFetch,
       enableXfa,
@@ -1684,6 +1685,7 @@ class PDFPageProxy {
       pdfBug: this._pdfBug,
       pageColors,
       enableHWA: this._transport.enableHWA,
+      enableWebGPU: this._transport.enableWebGPU,
       operationsFilter,
       rendererWorker: this._transport.rendererWorker,
     });
@@ -2605,6 +2607,7 @@ class WorkerTransport {
       styleElement: params.styleElement,
     });
     this.enableHWA = params.enableHWA;
+    this.enableWebGPU = params.enableWebGPU === true;
     this.rendererWorker = params.rendererWorker || null;
     this.loadingParams = params.loadingParams;
     this._params = params;
@@ -3555,6 +3558,7 @@ class InternalRenderTask {
     pdfBug = false,
     pageColors = null,
     enableHWA = false,
+    enableWebGPU = false,
     operationsFilter = null,
     rendererWorker = null,
   }) {
@@ -3588,6 +3592,7 @@ class InternalRenderTask {
     this._canvas = params.canvas;
     this._canvasContext = params.canvas ? null : params.canvasContext;
     this._enableHWA = enableHWA;
+    this._enableWebGPU = enableWebGPU;
     this._recordOperations = !!params.recordOperations;
     this._recordImages = !!params.recordImages;
     this._recordForDebugger = !!params.recordForDebugger;
@@ -3707,6 +3712,7 @@ class InternalRenderTask {
           pageId: this._pageId,
           renderTaskId: this._renderTaskId,
           enableHWA: this._enableHWA,
+          enableWebGPU: this._enableWebGPU,
           hasAnnotationCanvasMap: !!this.annotationCanvasMap,
           recordOperations: this._recordOperations,
           recordImages: this._recordImages,
--- src/display/renderer_worker.js
+++ src/display/renderer_worker.js
@@ -21,6 +21,7 @@ import {
 import { CanvasGraphics, getAnnotationCanvasName } from "./canvas.js";
 import { isNodeJS, setVerbosityLevel } from "../shared/util.js";
 import { FontLoader } from "./font_loader.js";
+import { initGPU } from "./webgpu.js";
 import { MessageHandler } from "../shared/message_handler.js";
 import { ObjectHandler } from "./object_handler.js";
 import { OffscreenCanvasFactory } from "./canvas_factory.js";
@@ -301,6 +302,7 @@ class RendererMessageHandler {
         pageId,
         renderTaskId,
         enableHWA = false,
+        enableWebGPU = false,
         hasAnnotationCanvasMap = false,
         transform,
         viewport,
@@ -333,6 +335,12 @@ class RendererMessageHandler {
       this.#renderTaskStates.set(renderTaskId, renderTaskState);
 
       try {
+        if (enableWebGPU) {
+          await initGPU();
+          if (renderTaskState.aborted) {
+            return;
+          }
+        }
         const objs = this.#getPageObjs(pageId);
         const optionalContentConfig = OptionalContentConfig.fromSerializable(
           data.optionalContentConfig
