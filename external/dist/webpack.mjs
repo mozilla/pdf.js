48cf356b826b9c52e3f003422cc6ebcbd5581c5f
Aditi <aditisingh1400@gmail.com>
2026-09-07 14:49:53 +0530

    fixup! [api-minor] Enable worker rendering

--- external/dist/webpack.mjs
+++ external/dist/webpack.mjs
@@ -21,10 +21,6 @@ if (typeof window !== "undefined" && "Worker" in window) {
     new URL("./build/pdf.worker.mjs", import.meta.url),
     { type: "module" }
   );
-  GlobalWorkerOptions.rendererSrc = new URL(
-    "./build/pdf.renderer.mjs",
-    import.meta.url
-  ).href;
 }
 
 export * from "./build/pdf.mjs";
