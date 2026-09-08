45c351d6a465fbbc28159ad043c7274e75e2f430
Jonas Jenwald <jonas.jenwald@gmail.com>
2026-09-08 16:20:37 +0200

    Convert the `missingGlyphs`, used in the `Font` class, into a Set
    This seems like a more appropriate data-structure, rather than storing `true` values in a regular Object.
    
    Also, fix existing inconsistencies in the `CMap.prototype.forEach` and `ToUnicodeMap.prototype.forEach` methods since they didn't always return integer charCodes. Note how various `forEach` call-sites previously did that manually, which seems like a "wrong" solution.

--- src/core/fonts.js
+++ src/core/fonts.js
@@ -1343,7 +1343,7 @@ class Font {
           this.toUnicode.forEach(function (charCode, unicodeCharCode) {
             const cid = map[charCode];
             if (cidToGidMap[cid] === undefined) {
-              map[+charCode] = unicodeCharCode;
+              map[charCode] = unicodeCharCode;
             }
           });
         }
@@ -1351,7 +1351,7 @@ class Font {
 
       if (!(this.toUnicode instanceof IdentityToUnicodeMap)) {
         this.toUnicode.forEach(function (charCode, unicodeCharCode) {
-          map[+charCode] = unicodeCharCode;
+          map[charCode] = unicodeCharCode;
         });
       }
       this.toFontChar = map;
@@ -1386,7 +1386,7 @@ class Font {
         !(this.toUnicode instanceof IdentityToUnicodeMap)
       ) {
         this.toUnicode.forEach(function (charCode, unicodeCharCode) {
-          map[+charCode] = unicodeCharCode;
+          map[charCode] = unicodeCharCode;
         });
       }
       this.toFontChar = map;
@@ -1402,7 +1402,7 @@ class Font {
             unicodeCharCode = unicode;
           }
         }
-        map[+charCode] = unicodeCharCode;
+        map[charCode] = unicodeCharCode;
       });
 
       // Attempt to improve the glyph mapping for (some) composite fonts that
@@ -2267,7 +2267,7 @@ class Font {
         locaEntries,
         numGlyphs
       );
-      const missingGlyphs = Object.create(null);
+      const missingGlyphs = new Set();
       let writeOffset = 0;
       itemEncode(locaData, 0, writeOffset);
       for (i = 0, j = itemSize; i < numGlyphs; i++, j += itemSize) {
@@ -2283,7 +2283,7 @@ class Font {
             );
         const newLength = glyphProfile.length;
         if (newLength === 0) {
-          missingGlyphs[i] = true;
+          missingGlyphs.add(i);
         }
         if (glyphProfile.sizeOfInstructions > maxSizeOfInstructions) {
           maxSizeOfInstructions = glyphProfile.sizeOfInstructions;
@@ -2963,7 +2963,7 @@ class Font {
 
     sanitizeHead(tables.head, numGlyphs, isTrueType ? tables.loca.length : 0);
 
-    let missingGlyphs = Object.create(null);
+    let missingGlyphs = new Set();
     if (isTrueType) {
       const glyphsInfo = sanitizeGlyphLocations(
         tables.loca,
@@ -3032,7 +3032,7 @@ class Font {
 
     // Helper function to try to skip mapping of empty glyphs.
     function hasGlyph(glyphId) {
-      return !missingGlyphs[glyphId];
+      return !missingGlyphs.has(glyphId);
     }
 
     if (properties.composite) {
